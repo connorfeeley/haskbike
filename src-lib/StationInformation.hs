00ca45a5f518f31eda8af28f9ab65f92fc13e50e
Connor Feeley <git@cfeeley.org>
2023-09-08 09:00:00 -0400

    StationInformation: convert to GADT syntax

--- src-lib/StationInformation.hs
+++ src-lib/StationInformation.hs
@@ -14,14 +14,15 @@ import qualified Data.Text    as Text
 import           GHC.Generics
 
 
--- | A simple type to represent a BikeShare station physical configuration.
-data PhysicalConfiguration = ElectricBikeStation
-                           | Regular
-                           | RegularLitMapFrame
-                           | SmartLitMapFrame
-                           | SmartMapFrame
-                           | Vault
-                           deriving (Show, Eq, Generic)
+-- | Enumeration representing a BikeShare station physical configuration.
+data PhysicalConfiguration where
+  ElectricBikeStation   :: PhysicalConfiguration
+  Regular               :: PhysicalConfiguration
+  RegularLitMapFrame    :: PhysicalConfiguration
+  SmartLitMapFrame      :: PhysicalConfiguration
+  SmartMapFrame         :: PhysicalConfiguration
+  Vault                 :: PhysicalConfiguration
+  deriving (Show, Eq, Generic)
 
 instance ToJSON PhysicalConfiguration where
   toJSON ElectricBikeStation = String (Text.pack "ELECTRICBIKESTATION")
@@ -41,48 +42,50 @@ instance FromJSON PhysicalConfiguration where
      "VAULT"               -> return Vault
      _                     -> fail ("Invalid PhysicalConfiguration: " ++ show t)
 
--- | A simple type to represent a BikeShare rental method.
-data RentalMethod = Key
-                  | TransitCard
-                  | CreditCard
-                  | Phone
-                  deriving (Show, Eq, Generic)
+-- | Enumeration representing a BikeShare rental method.
+data RentalMethod where
+  Key           :: RentalMethod
+  TransitCard   :: RentalMethod
+  CreditCard    :: RentalMethod
+  Phone         :: RentalMethod
+  deriving (Show, Eq, Generic)
 
 instance ToJSON RentalMethod where
-    toJSON Key         = String "KEY"
-    toJSON TransitCard = String "TRANSITCARD"
-    toJSON CreditCard  = String "CREDITCARD"
-    toJSON Phone       = String "PHONE"
+  toJSON Key         = String "KEY"
+  toJSON TransitCard = String "TRANSITCARD"
+  toJSON CreditCard  = String "CREDITCARD"
+  toJSON Phone       = String "PHONE"
 
 instance FromJSON RentalMethod where
-    parseJSON = withText "RentalMethod" $ \t ->
-        case Text.toUpper t of
-            "KEY"         -> return Key
-            "TRANSITCARD" -> return TransitCard
-            "CREDITCARD"  -> return CreditCard
-            "PHONE"       -> return Phone
-            _             -> fail $ "Invalid RentalMethod: " ++ Text.unpack t
+  parseJSON = withText "RentalMethod" $ \t ->
+    case Text.toUpper t of
+      "KEY"         -> return Key
+      "TRANSITCARD" -> return TransitCard
+      "CREDITCARD"  -> return CreditCard
+      "PHONE"       -> return Phone
+      _             -> fail $ "Invalid RentalMethod: " ++ Text.unpack t
 
--- | A simple type to represent a BikeShare station.
-data Station = Station
-  { station_id             :: String
-  , name                   :: String
-  , physical_configuration :: PhysicalConfiguration
-  , lat                    :: Double
-  , lon                    :: Double
-  , altitude               :: Double
-  , address                :: String
-  , capacity               :: Int
-  , is_charging_station    :: Bool
-  , rental_methods         :: [RentalMethod]
-  , is_virtual_station     :: Bool
-  , groups                 :: [String]
-  , obcn                   :: String -- On-board communication number (phone number used by station)
-  , nearby_distance        :: Double
-  , bluetooth_id           :: String
-  , ride_code_support      :: Bool
-  , rental_uris            :: Object
-  } deriving (Show, Eq, Generic)
+-- | A type representing a BikeShare station.
+data Station where
+  Station :: { station_id :: String
+             , name :: String
+             , physical_configuration :: PhysicalConfiguration
+             , lat :: Double
+             , lon :: Double
+             , altitude :: Double
+             , address :: String
+             , capacity :: Int
+             , is_charging_station :: Bool
+             , rental_methods :: [RentalMethod]
+             , is_virtual_station :: Bool
+             , groups :: [String]
+             , obcn :: String
+             , nearby_distance :: Double
+             , bluetooth_id :: String
+             , ride_code_support :: Bool
+             , rental_uris :: Object
+             } -> Station
+  deriving (Show, Eq, Generic)
 
 instance ToJSON Station where
   toJSON station =
