373f9a2b0cb9757b8abfcecf9bd3186cd19d3145
Connor Feeley <git@cfeeley.org>
2023-09-12 09:00:00 -0400

    BikeShareAPI: use API.Types

--- src-lib/BikeShareAPI.hs
+++ src-lib/BikeShareAPI.hs
@@ -7,19 +7,18 @@ module BikeShareAPI
   ( BikeShareAPI
   ) where
 
-import           Data.Aeson         (Object)
-import           Servant.API
+import           API.Types   (StationInformationResponse, StationStatusResponse)
 
-import qualified StationInformation as SI
-import qualified StationStatus      as SS
+import           Data.Aeson  (Object)
+import           Servant.API
 
 
 -- | The Bike Share API.
 type BikeShareAPI =
   "gbfs_versions"                             :> Get '[JSON] Object
   :<|> "en" :> "vehicle_types"                :> Get '[JSON] Object
-  :<|> "en" :> "station_information"          :> Get '[JSON] SI.StationInformationResponse
-  :<|> "en" :> "station_status"               :> Get '[JSON] SS.StationStatusResponse
+  :<|> "en" :> "station_information"          :> Get '[JSON] StationInformationResponse
+  :<|> "en" :> "station_status"               :> Get '[JSON] StationStatusResponse
   :<|> "en" :> "system_regions"               :> Get '[JSON] Object
   :<|> "en" :> "system_information"           :> Get '[JSON] Object
   :<|> "en" :> "system_pricing_plans"         :> Get '[JSON] Object
