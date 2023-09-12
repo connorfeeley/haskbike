dedb2a856f7aeb40fa7e1d0e5fc6282caaaa9de7
Connor Feeley <git@cfeeley.org>
2023-09-12 09:00:00 -0400

    API.Types: export Station{Information,Status}

---
+++ src-lib/API/Types.hs
@@ -0,0 +1,9 @@
+-- | This module re-exports all the types used in the API.
+
+module API.Types
+    ( module StationInformation
+    , module StationStatus
+    ) where
+
+import           StationInformation
+import           StationStatus
