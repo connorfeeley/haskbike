09f6de5f6d81ed84db6ac78e70da919e594b72c0
Connor Feeley <git@cfeeley.org>
2023-09-20 09:00:00 -0400

    Common: add helper to convert reportTime to system time

--- src-lib/Common.hs
+++ src-lib/Common.hs
@@ -4,6 +4,7 @@ module Common
   ( reportTimeZone
   , posixToLocal
   , localToPosix
+  , localToSystem
   ) where
 
 import           Data.Time             (LocalTime, TimeZone (..),
@@ -12,6 +13,19 @@ import           Data.Time             (LocalTime, TimeZone (..),
                                         utcToLocalTime)
 import           Data.Time.Clock.POSIX (posixSecondsToUTCTime,
                                         utcTimeToPOSIXSeconds)
+import Data.Time.LocalTime (getCurrentTimeZone)
+
+
+localToSystem :: LocalTime -> IO LocalTime
+localToSystem time = do
+  -- Get the current timezone
+  currentTimeZone <- getCurrentTimeZone
+  -- Convert the local time to a POSIX time, using "fake UTC" as the timezone
+  let asPosix = localToPosix time
+  -- Convert the POSIX time to a local time, using the system's current timezone
+  pure $ posixToLocal' currentTimeZone asPosix
+  where
+    posixToLocal' timezone = utcToLocalTime timezone . posixSecondsToUTCTime . secondsToNominalDiffTime . fromIntegral
 
 -- | TimeZone used to convert the API.T.StationStatus.last_reported field to a local time (effectively UTC).
 reportTimeZone :: TimeZone
