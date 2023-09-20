23315708d95585f5cf142897017b32321eba536b
Connor Feeley <git@cfeeley.org>
2023-09-20 09:00:00 -0400

    Poll: set interval from response consumer

--- src-lib/API/Poll.hs
+++ src-lib/API/Poll.hs
@@ -9,11 +9,9 @@ import           Common
 
 import           Servant.Client
 
-import           Control.Applicative             ((<|>))
 import           Control.Concurrent              (forkIO, threadDelay)
 import           Control.Concurrent.STM
 import           Control.Concurrent.STM.TBMQueue
-import           Control.Exception               (finally)
 import           Control.Monad                   (forever, void)
 import           Data.Foldable                   (for_)
 import           UnliftIO.Async
@@ -21,40 +19,31 @@ import           UnliftIO.Async
 
 main :: IO ()
 main = do
+  ttl <- newTVarIO (1 * 1000000)
   queue <- newTBMQueueIO 4
   queue_resp <- newTBMQueueIO 4
 
   -- Request status every second.
-  fillQueueThread queue 1000000
+  void $ forkIO $ forever $ fillQueue queue ttl
 
-  void $
+  concurrently_
     -- Drain request queue indefinitely.
-    withAsync (drainRequestQueue queue queue_resp) $ \req_t ->
+    (drainRequestQueue queue queue_resp)
     -- Drain response queue indefinitely.
-    withAsync (drainResponseQueue queue_resp) $ \resp_t ->
-    atomically $ waitSTM req_t <|> waitSTM resp_t
-
-
--- | Enqueue queries forever.
-fillQueueThread:: TBMQueue (ClientM StationStatusResponse) -> Int -> IO ()
-fillQueueThread queue interval = void $ forkIO $ forever $ do
-  threadDelay interval
-  fillQueue queue
+    (drainResponseQueue queue_resp ttl)
 
 
 -- | Enqueue one query.
-fillQueue:: TBMQueue (ClientM StationStatusResponse) -> IO ()
-fillQueue queue = do
+fillQueue:: TBMQueue (ClientM StationStatusResponse) -> TVar Int -> IO ()
+fillQueue queue interval_var = do
+  -- Wait for the interval to pass.
+  interval <- readTVarIO interval_var
+  putStrLn $ "FILL: interval=" ++ show (interval `div` 1000000) ++ "s"
+  threadDelay interval
+
   let queries = [stationStatus]
   for_ queries $ \query ->
     atomically $ writeTBMQueue queue query
-
-
--- | Drain request queue forever.
-drainRequestQueueThread :: TBMQueue (ClientM StationStatusResponse) -> TBMQueue StationStatusResponse -> IO ()
-drainRequestQueueThread queue queue_resp = void $ forkIO $ forever $ do
-  putStrLn "Draining request queue indefinitely"
-  drainRequestQueue queue queue_resp
 
 
 -- | Drain request queue once.
@@ -70,9 +59,9 @@ drainRequestQueue queue queue_resp =
         Just query -> do
           response <- runQueryWithEnv query
           case response of
-            Left  err  -> putStrLn $ "Error: " ++ show err
+            Left  err  -> putStrLn $ "REQUEST: error: " ++ show err
             Right result -> do
-              putStrLn "Got response"
+              putStrLn "REQUEST: enqueing response"
 
               -- Enqueue the response.
               atomically $ writeTBMQueue queue_resp result
@@ -81,16 +70,9 @@ drainRequestQueue queue queue_resp =
               loop
 
 
--- | Drain response queue indefinitely.
-drainResponseQueueThread :: TBMQueue StationStatusResponse -> IO ()
-drainResponseQueueThread queue = void $ forkIO $ forever $ do
-  putStrLn "Draining response queue indefinitely"
-  drainResponseQueue queue
-
-
 -- | Drain response queue once.
-drainResponseQueue :: TBMQueue StationStatusResponse -> IO ()
-drainResponseQueue queue =
+drainResponseQueue :: TBMQueue StationStatusResponse -> TVar Int -> IO ()
+drainResponseQueue queue interval_val =
     loop
   where
     loop = do
@@ -100,8 +82,11 @@ drainResponseQueue queue =
         Nothing -> atomically retry
         Just response -> do
           time  <- localToSystem $ response_last_updated response
-          putStrLn $ "Last updated " ++ show time
-          putStrLn $ "TTL: " ++ show (response_ttl response)
+          putStrLn $ "RESPONSE: TTL=" ++ show (response_ttl response) ++ " | " ++ "last updated=" ++ show time
+
+          -- Update the interval
+          -- FIXME: this ends up updating the *next* interval, not the current one.
+          atomically $ writeTVar interval_val ((response_ttl response + 1) * 1000000)
 
           -- Restart loop
           loop
