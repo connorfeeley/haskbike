44f2fcbfd47ebe14407c78f34251d44d0eb1bbcd
Connor Feeley <git@cfeeley.org>
2023-09-22 09:00:00 -0400

    Poll: correctly polling

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -2,15 +2,42 @@ module Main
      ( main
      ) where
 
-import qualified API.Poll       as P
+import           API.Client
+import qualified API.Poll            as P
+import           API.ResponseWrapper
+import           API.Types
 
+import           Control.Lens
+
+import           Database.Operations
 import           Database.Utils
+
+import           Servant.Client      ( ClientError )
 
 
 main :: IO ()
 main = do
   -- Setup the database.
   conn <- setupDatabaseName dbnameProduction
+
+  -- Request station information and status from API.
+  info   <- runQueryWithEnv stationInformation  :: IO (Either ClientError StationInformationResponse)
+  status <- runQueryWithEnv stationStatus       :: IO (Either ClientError StationStatusResponse)
+  
+  -- Insert station information into database.
+  case info of
+    Left  err  -> putStrLn $ "MAIN (ERROR): " ++ show err
+    Right response -> do
+      inserted <- insertStationInformation conn $ response ^. response_data . info_stations
+      putStrLn $ "MAIN:     inserted info " ++ show (length inserted)
+
+  -- Insert station status into database.
+  case status of
+    Left  err  -> putStrLn $ "REQUEST: error: " ++ show err
+    Right response -> do
+      inserted <- insertUpdatedStationStatus conn $ response ^. response_data . status_stations
+      putStrLn $ "MAIN:    inserted status " ++ show (length $ inserted ^. insert_inserted)
+      putStrLn $ "MAIN: deactivated status " ++ show (length $ inserted ^. insert_deactivated)
 
   -- Run API poller method.
   P.pollClient conn
--- src-lib/API/Poll.hs
+++ src-lib/API/Poll.hs
@@ -15,15 +15,24 @@ import           API.Types                       ( StationStatusResponse, status
 
 import           Common
 
+import           Control.Applicative             ( Applicative (liftA2), liftA )
 import           Control.Concurrent              ( threadDelay )
 import           Control.Concurrent.STM
 import           Control.Concurrent.STM.TBMQueue
 import           Control.Lens
+import           Control.Monad                   ( when )
+
+import           Data.Maybe                      ( listToMaybe )
+import           Data.Time
+import           Data.Time.Clock.POSIX
 
 import           Database.Beam.Postgres          ( Connection )
-import           Database.BikeShare              ( d_status_last_reported, d_status_station_id )
+import           Database.BikeShare              ( BeamReportTime (BeamReportTime), d_status_last_reported,
+                                                   d_status_station_id )
 import           Database.Operations
 import           Database.Utils
+
+import           Text.Pretty.Simple              ( pPrint, pPrintString )
 
 import           UnliftIO.Async
 
@@ -37,15 +46,16 @@ main = do
 
 pollClient :: Connection -> IO ()
 pollClient conn = do
-  ttl <- newTVarIO (0 * 1000000)
-  queue_resp <- newTBMQueueIO 4
+  ttl           <- newTVarIO (0 * 1000000)
+  last_updated  <- newTVarIO 0
+  queue_resp    <- newTBMQueueIO 4
 
   -- Request status every second.
   -- void $ forkIO $ forever $ fillQueue queue ttl
 
   concurrently_
     (statusRequester queue_resp ttl)
-    (statusHandler conn queue_resp)
+    (statusHandler conn queue_resp last_updated)
 
 statusRequester :: TBMQueue StationStatusResponse -> TVar Int -> IO ()
 statusRequester queue interval_var = loop'
@@ -72,8 +82,11 @@ statusRequester queue interval_var = loop'
 
           loop' -- Restart loop
 
-statusHandler :: Connection -> TBMQueue StationStatusResponse -> IO ()
-statusHandler conn queue =
+statusHandler :: Connection                     -- ^ Database connection
+              -> TBMQueue StationStatusResponse -- ^ Queue of responses
+              -> TVar Int                       -- ^ Last updated time
+              -> IO ()
+statusHandler conn queue last_updated =
   loop'
   where
     loop' = do
@@ -81,16 +94,34 @@ statusHandler conn queue =
       case mnext of
         Nothing -> atomically retry
         Just response -> do
-          let resp_data = response ^. response_data
-          let status = resp_data ^. status_stations
+          let currentTime = localToPosix $ response ^. response_last_updated
+
+          previousTime <- readTVarIO last_updated
+
+          let timeElapsed = currentTime - previousTime
+
+          -- Check if last_updated went backwards
+          case timeElapsed > 0 of
+            True  ->
+              -- Update last_updated variable.
+              atomically $ writeTVar last_updated currentTime
+            False -> do
+              putStrLn $ "HANDLER: last_updated went backwards: " ++ show previousTime ++ " -> " ++ show currentTime ++ "(" ++ show timeElapsed ++ ")"
+
+          let status = response ^. (response_data . status_stations)
           putStrLn $ "HANDLER: status_stations=" ++ show (length status)
 
           updated_api <- separateNewerStatusRecords conn status
-          putStrLn $ "HANDLER: updated_api="  ++ show (length $ updated_api ^. filter_newer)
-          putStrLn $ "HANDLER: same_api=" ++ show (length $ updated_api ^. filter_unchanged)
+          -- pPrint $ listToMaybe status
+          -- pPrint $ listToMaybe $ updated_api ^. filter_newer
+          -- pPrint $ listToMaybe $ updated_api ^. filter_unchanged
+
+          putStrLn $ "HANDLER:     newer="  ++ show (length $ updated_api ^. filter_newer)
+          putStrLn $ "HANDLER: unchanged="     ++ show (length $ updated_api ^. filter_unchanged)
 
           -- Insert the updated status.
-          inserted_result <- insertUpdatedStationStatus conn status
+          inserted_result <- insertUpdatedStationStatus conn $ updated_api ^. filter_newer
+
           let message_data = zipWith (\ updated inserted ->
                                  ( inserted ^.. d_status_station_id
                                  , updated  ^.. d_status_last_reported
