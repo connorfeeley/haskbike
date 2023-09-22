57eb11d1882ca6c29c381ca7fce98d512514b631
Connor Feeley <git@cfeeley.org>
2023-09-22 09:00:00 -0400

    exe: fill database with initial data if empty

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -23,24 +23,32 @@ main = do
   -- Connect to database without resetting.
   conn <- connectDbName dbnameProduction
 
-  -- Request station information and status from API.
-  info   <- runQueryWithEnv stationInformation  :: IO (Either ClientError StationInformationResponse)
-  status <- runQueryWithEnv stationStatus       :: IO (Either ClientError StationStatusResponse)
-  
-  -- Insert station information into database.
-  case info of
-    Left  err  -> putStrLn $ "MAIN (ERROR): " ++ show err
-    Right response -> do
-      inserted <- insertStationInformation conn $ response ^. response_data . info_stations
-      putStrLn $ "MAIN:     inserted info " ++ show (length inserted)
+  -- Insert station information if missing from database.
+  queryStationInformation conn >>= \info_rows ->
+    case length info_rows of
+      0 -> do -- Request station information from API.
+        info   <- runQueryWithEnv stationInformation  :: IO (Either ClientError StationInformationResponse)
+        -- Insert station information into database.
+        case info of
+          Left  err  -> putStrLn $ "MAIN (ERROR): " ++ show err
+          Right response -> do
+            inserted <- insertStationInformation conn $ response ^. response_data . info_stations
+            putStrLn $ "MAIN:     inserted info " ++ show (length inserted)
+      _ -> putStrLn "MAIN:     station information already present"
 
-  -- Insert station status into database.
-  case status of
-    Left  err  -> putStrLn $ "REQUEST: error: " ++ show err
-    Right response -> do
-      inserted <- insertUpdatedStationStatus conn $ response ^. response_data . status_stations
-      putStrLn $ "MAIN:    inserted status " ++ show (length $ inserted ^. insert_inserted)
-      putStrLn $ "MAIN: deactivated status " ++ show (length $ inserted ^. insert_deactivated)
+  -- Insert station status if missing from database.
+  queryStationStatus conn >>= \status_rows ->
+    case length status_rows of
+      0 -> do -- Request station status from API.
+        status <- runQueryWithEnv stationStatus       :: IO (Either ClientError StationStatusResponse)
+        -- Insert station status into database.
+        case status of
+          Left  err  -> putStrLn $ "REQUEST: error: " ++ show err
+          Right response -> do
+            inserted <- insertUpdatedStationStatus conn $ response ^. response_data . status_stations
+            putStrLn $ "MAIN:    inserted status " ++ show (length $ inserted ^. insert_inserted)
+            putStrLn $ "MAIN: deactivated status " ++ show (length $ inserted ^. insert_deactivated)
+      _ -> putStrLn "MAIN:    station status already present"
 
   -- Run API poller method.
   P.pollClient conn
