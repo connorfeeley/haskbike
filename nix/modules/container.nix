5f1023ee08ccde8939e817dea2c0891aa355e7f3
Connor Feeley <git@cfeeley.org>
2023-09-23 09:00:00 -0400

    nix: add vm nixosConfigurations

--- nix/modules/container.nix
+++ nix/modules/container.nix
@@ -15,7 +15,11 @@ in
 {
   config = {
     flake = rec {
-      nixosModules.container = moduleWithSystem (
+      nixosModules.vm = { ... }: {
+        # Make VM output to the terminal instead of a separate window
+        virtualisation.vmVariant.virtualisation.graphics = false;
+      };
+      nixosModules.haskbike = moduleWithSystem (
         perSystem@{ config, pkgs }: # NOTE: only explicit params will be in perSystem
         nixos@{ ... }:
         {
@@ -25,9 +29,14 @@ in
           # of this flake.
           system.configurationRevision = pkgs.lib.mkIf (self ? rev) self.rev;
 
+          # Set NixOS state version to the latest.
+          system.stateVersion = "23.05";
+
           # Network configuration.
           networking.useDHCP = false;
           networking.firewall.allowedTCPPorts = [ 80 ];
+
+          environment.systemPackages = [ config.packages.haskbike ];
 
           # Enable a web server.
           services.httpd = {
@@ -47,7 +56,9 @@ in
             inherit inputs inputs';
           };
           modules = [
-            nixosModules.container
+            # Import the module defined above.
+            nixosModules.haskbike
+
             # This module could be moved into a separate file; otherwise we might
             # as well have used ctx.config.packages directly.
             ({ config, lib, packages, pkgs, ... }: {
@@ -56,6 +67,52 @@ in
             })
           ];
         });
+      nixosConfigurations.vm = withSystem "x86_64-linux" (ctx@{ config, inputs', system, ... }:
+        inputs.nixpkgs.lib.nixosSystem {
+          # Expose `packages`, `inputs` and `inputs'` as module arguments.
+          # Use specialArgs permits use in `imports`.
+          # Note: if you publish modules for reuse, do not rely on specialArgs, but
+          # on the flake scope instead. See also https://flake.parts/define-module-in-separate-file.html
+          specialArgs = {
+            packages = config.packages;
+            inherit inputs inputs';
+          };
+          modules = [
+            # Import the module defined above.
+            nixosModules.vm
+
+            # This module could be moved into a separate file; otherwise we might
+            # as well have used ctx.config.packages directly.
+            ({ config, lib, packages, pkgs, ... }: {
+              imports = [ ];
+              nixpkgs.hostPlatform = system;
+            })
+          ];
+        });
+      nixosConfigurations.darwin-vm = withSystem "aarch64-linux" (ctx@{ config, inputs', system, ... }:
+        inputs.nixpkgs.lib.nixosSystem {
+          # Expose `packages`, `inputs` and `inputs'` as module arguments.
+          # Use specialArgs permits use in `imports`.
+          # Note: if you publish modules for reuse, do not rely on specialArgs, but
+          # on the flake scope instead. See also https://flake.parts/define-module-in-separate-file.html
+          specialArgs = {
+            packages = config.packages;
+            inherit inputs inputs';
+          };
+          modules = [
+            # Import the module defined above.
+            nixosModules.vm
+
+            # This module could be moved into a separate file; otherwise we might
+            # as well have used ctx.config.packages directly.
+            ({ config, lib, packages, pkgs, inputs, ... }: {
+              imports = [ ];
+              nixpkgs.hostPlatform = system;
+              virtualisation.vmVariant.virtualisation.host.pkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
+            })
+          ];
+        });
+      packages.aarch64-darwin.darwinVM = self.nixosConfigurations.darwinVM.config.system.build.vm;
     };
   };
 }
