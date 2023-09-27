f01bffc9766334c19b2b1bb7b6ee13bb84a1a75d
Connor Feeley <git@cfeeley.org>
2023-09-27 09:00:00 -0400

    exe: add subcommands and subcommand options

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -37,45 +37,54 @@ import           System.Exit            ( exitSuccess )
 
 import           UnliftIO               ( MonadUnliftIO )
 
-data Command where
-  Poll :: Command
-  Reset :: Command
-  deriving (Show)
-
+-- | Top-level options.
 data Options where
   Options :: { optCommand :: !Command
              , optVerbose :: Bool
-             , optInputFile :: FilePath
-             , optOutputFile :: FilePath
              } -> Options
   deriving (Show)
 
-parseCommand :: Parser Command
-parseCommand =
-  flag' Poll
-  ( long "poll"
-    <> help "Run Poll command")
-  <|> flag' Reset
-  ( long "reset"
-    <> help "Run Reset command")
-
+-- | Parser for 'Options'.
 parseOptions :: Parser Options
 parseOptions = Options
-  <$> parseCommand
+  <$> commandParser
   <*> switch
       ( long "verbose"
      <> short 'v'
      <> help "Enable verbose output" )
-  <*> strOption
-      ( long "input"
-     <> short 'i'
-     <> metavar "FILE"
-     <> help "Input file" )
-  <*> strOption
-      ( long "output"
-     <> short 'o'
-     <> metavar "FILE"
-     <> help "Output file" )
+
+-- | Top-level commands.
+data Command where
+  Poll  :: Command
+  Reset :: !ResetOptions -> Command
+  deriving (Show)
+
+-- | Parser for 'Command'.
+commandParser :: Parser Command
+commandParser = subparser
+  (  command "poll"   (info (pure Poll)
+                       (progDesc "Poll the API and insert new station status into database."))
+  <> command "reset" (info (Reset <$> resetOptionsParser)
+                      (progDesc "Reset the database. [DANGER]"))
+  )
+
+-- | Options for the 'Reset' command.
+data ResetOptions where
+  ResetOptions :: { optResetOnly :: Bool
+                  , optTest      :: Bool
+                  } -> ResetOptions
+  deriving (Show)
+
+-- | Parser for 'ResetOptions'.
+resetOptionsParser :: Parser ResetOptions
+resetOptionsParser = ResetOptions
+  <$> switch
+      ( long "reset-only"
+     <> help "Only reset, don't insert new data" )
+  <*> switch
+      ( long "test"
+     <> help "Run the command in test mode" )
+
 
 main :: IO ()
 main = runApp simpleEnv appMain
@@ -83,7 +92,7 @@ main = runApp simpleEnv appMain
 appMain :: (WithLog env Message m, MonadIO m, MonadUnliftIO m)
         => m ()
 appMain = do
-  options <- liftIO $ customExecParser (prefs helpShowGlobals) opts
+  options <- liftIO $ customExecParser (prefs $ helpShowGlobals <> showHelpOnEmpty <> showHelpOnError) opts
   liftIO $ print options
 
   -- Get command-line arguments
