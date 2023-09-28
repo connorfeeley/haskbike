9c35ee94499face17db76445f9841ae4ee43692c
Connor Feeley <git@cfeeley.org>
2023-09-28 09:00:00 -0400

    exe: log to info by default

--- src-lib/AppEnv.hs
+++ src-lib/AppEnv.hs
@@ -7,9 +7,10 @@ module AppEnv
      , Env (..)
      , runApp
      , simpleEnv
+      , mainEnv
      ) where
 
-import           Colog                  ( HasLog (..), LogAction, Message, richMessageAction )
+import           Colog                  ( HasLog (..), LogAction(..), Message, richMessageAction, filterBySeverity, Severity (..), Msg (msgSeverity))
 
 import           Control.Monad.IO.Class ( MonadIO )
 import           Control.Monad.Reader   ( MonadReader, ReaderT (..) )
@@ -20,27 +21,43 @@ import           UnliftIO               ( MonadUnliftIO )
 
 -- Application environment
 data Env m where
-  Env :: { envLogAction :: !(LogAction m Message) } -> Env m
+  Env :: { envLogAction   :: !(LogAction m Message)
+         , envMinSeverity :: !Severity
+         } -> Env m
 
 -- Implement logging for the application environment.
 instance HasLog (Env m) Message m where
-    getLogAction :: Env m -> LogAction m Message
-    getLogAction = envLogAction
-    {-# INLINE getLogAction #-}
+  getLogAction :: Env m -> LogAction m Message
+  getLogAction = envLogAction
+  {-# INLINE getLogAction #-}
 
-    setLogAction :: LogAction m Message -> Env m -> Env m
-    setLogAction newLogAction env = env { envLogAction = newLogAction }
-    {-# INLINE setLogAction #-}
+  setLogAction :: LogAction m Message -> Env m -> Env m
+  setLogAction newLogAction env = env { envLogAction = newLogAction }
+  {-# INLINE setLogAction #-}
 
 -- Application type
 newtype App a = App
-    { unApp :: ReaderT (Env App) IO a
-    } deriving newtype (Functor, Applicative, Monad, MonadIO, MonadUnliftIO, MonadReader (Env App))
+  { unApp :: ReaderT (Env App) IO a
+  } deriving newtype (Functor, Applicative, Monad, MonadIO, MonadUnliftIO, MonadReader (Env App))
 
+-- | Simple environment for the main application.
 simpleEnv :: Env App
-simpleEnv = Env
-    { envLogAction  = richMessageAction
-    }
+simpleEnv = Env { envLogAction   = mainLogAction Info
+                , envMinSeverity = Info
+                }
 
+-- | Environment for the main application.
+mainEnv :: Severity -> Env App
+mainEnv sev = Env { envLogAction   = mainLogAction sev
+                  , envMinSeverity = Info
+                  }
+
+-- | Log action for the main application.
+mainLogAction :: (MonadIO m)
+              => Severity            -- ^ Severity level (verbosity).
+              -> LogAction m Message -- ^ Action used to log.
+mainLogAction severity = filterBySeverity severity msgSeverity richMessageAction
+
+  -- | Helper function to run the application.
 runApp :: Env App -> App a -> IO a
 runApp env app = runReaderT (unApp app) env
