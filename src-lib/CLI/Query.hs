8f91dd2f6e78c6914f5ec66bd0d1cc59188f3303
Connor Feeley <git@cfeeley.org>
2023-10-03 09:00:00 -0400

    cli(query): unify query interface

--- src-lib/CLI/Query.hs
+++ src-lib/CLI/Query.hs
@@ -1,10 +1,6 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE DerivingStrategies  #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE PatternSynonyms     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections       #-}
 
 -- | CLI interface for querying the database.
 module CLI.Query
@@ -20,6 +16,7 @@ import           Control.Lens
 
 import           Data.Int               ( Int32 )
 import qualified Data.List              as List
+import           Data.Maybe             ( fromMaybe )
 import           Data.Text.Lazy         ( Text, intercalate, pack, toStrict, unlines, unpack )
 import           Data.Time              ( LocalTime (..), TimeZone, defaultTimeLocale, formatTime, getCurrentTimeZone )
 
@@ -42,6 +39,8 @@ import           System.Console.ANSI
 
 import           UnliftIO               ( MonadIO, MonadUnliftIO, liftIO )
 
+
+-- | Dispatch CLI arguments to the query interface.
 dispatchQuery :: (WithLog env Message m, MonadIO m, MonadUnliftIO m)
               => QueryOptions
               -> Connection
@@ -52,23 +51,32 @@ dispatchQuery options conn = do
     QueryByStationName stationName -> queryByStationName stationName conn
 
 
+-- | Query the database for the station with the given ID.
 queryByStationId :: (WithLog env Message m, MonadIO m, MonadUnliftIO m)
                  => Int
                  -> Connection
                  -> m ()
 queryByStationId stationId conn = do
   log I $ toStrict $ "Querying station ID '" <> (pack . show) stationId <> "'"
-  name <- liftIO $ queryStationName conn stationId
-  log I $ toStrict $ "Station : " <> (pack . show) name
-  pPrintCompact name
 
+  -- Try to lookup station name from the ID.
+  name <- liftIO $ queryStationName conn stationId
+  -- Maybe tuple of (id, name)
+  let station_pair = fmap (stationId,) name
+
+  log I $ toStrict $ "Station: " <> (pack . show) station_pair
+
+  results <- mapM (queryStatus conn "By ID") station_pair
+
+  liftIO $ putStrLn $ unpack $ unlines (fromMaybe [] results)
+
+
+-- | Query the database for stations with names matching the given pattern.
 queryByStationName :: (WithLog env Message m, MonadIO m, MonadUnliftIO m)
                    => MatchMethod String
                    -> Connection
                    -> m ()
 queryByStationName stationMatch conn = do
-  currentTimeZone <- liftIO getCurrentTimeZone
-
   log I $ toStrict $ "Querying station names like '" <> pack stationName <> "'"
 
   let transformer = case stationMatch of
@@ -80,21 +88,37 @@ queryByStationName stationMatch conn = do
   results <- liftIO $ queryStationIdLike conn (snd transformer)
   log D $ toStrict $ fst transformer <> ": "    <> (pack . show) results
 
-  resultsText  <- liftIO $ mapM (fmtStationStatus conn currentTimeZone) results
-  liftIO $ putStrLn $ unpack $ unlines $ withHeader (fst transformer)  (concat resultsText)
+  resultsText <- mapM (queryStatus conn (unpack $ fst transformer)) results
+  liftIO $ putStrLn $ unpack $ unlines (concat resultsText)
 
   where
     stationName = unMatchMethod stationMatch
+    nameTransformer prepend name append = List.intercalate "" [prepend, name, append]
+
+-- | Query the database for the status of the given station tuple.
+queryStatus :: (WithLog env Message m, MonadIO m, MonadUnliftIO m)
+                   => Connection
+                   -> String -- ^ Header
+                   -> (Int, String) -- ^ (station_id, station_name)
+                   -> m [Text]
+queryStatus conn header station_tuple = do
+  -- Query the current time zone.
+  currentTimeZone <- liftIO getCurrentTimeZone
+
+  -- Query the latest status for the given station tuple.
+  resultsText <- liftIO (fmtStationStatus conn currentTimeZone station_tuple)
+  pure $ withHeader (pack header) resultsText
+
+  where
     withHeader :: Text -> [Text] -> [Text]
-    withHeader header results = case results of
+    withHeader header' results = case results of
       [] -> fmtHeader : ["\t\t" <> indent 6 <> italCode <> " None." <> resetItal ]
       _  -> fmtHeader : results
       where
-        fmtHeader = "\t" <> indent 5 <> indent 8 <> " " <> boldCode <> underCode <> header <> resetUnder <> resetIntens <> " " <> indent 8
+        fmtHeader = "\t" <> indent 5 <> indent 8 <> " " <> boldCode <> underCode <> header' <> resetUnder <> resetIntens <> " " <> indent 8
         indent (n :: Int) = pack (unwords (replicate n ""))
         boxLine n = intercalate "" (replicate n boxDrawingCharacter)
         boxDrawingCharacter = "\x2500" :: Text -- Unicode U+2500 (Box Drawing Character)
-    nameTransformer prepend name append = List.intercalate "" [prepend, name, append]
 
     fmtStationStatus :: Connection -> TimeZone -> (Int, String) -> IO [Text]
     fmtStationStatus conn' currentTimeZone' (id', name') = do
@@ -128,7 +152,7 @@ formattedLastReport :: TimeZone -> Maybe LocalTime -> Text
 formattedLastReport timeZone status = reportedText
   where
     reportedText = case status of
-      Nothing -> boldCode <> colouredText' Vivid Red "Never" <> resetIntens
+      Nothing -> boldCode <> colouredText Vivid Red "Never" <> resetIntens
       -- Just t  -> italCode <> showText t <> resetItal
       Just t  -> "[" <> showText t <> "]"
               <> boldCode <> "\t" <> "|" <> "\t" <> resetIntens
@@ -141,24 +165,21 @@ showText = pack . show
 
 fmtAvailability :: (Text, Int32, Int32) -> Text
 fmtAvailability (name, avail, disabled)
-  = colouredText' Dull Yellow name
- <> colouredText' Vivid Green  (fmt $ padLeftF 2 ' ' avail)    <> " available" <> tab
+  = colouredText Dull Yellow name
+ <> colouredText Vivid Green  (fmt $ padLeftF 2 ' ' avail)    <> " available" <> tab
  <> boldCode <> "|" <> tab <> resetIntens
- <> colouredText' Vivid Red    (fmt $ padLeftF 2 ' ' disabled) <> " disabled"
+ <> colouredText Vivid Red    (fmt $ padLeftF 2 ' ' disabled) <> " disabled"
  where tab = "\t" :: Text
 
 fmtBikeAvailability :: Text -> Int32 -> Text
 fmtBikeAvailability name count
   = tab
- <> colouredText' Dull Yellow (fmt $ padLeftF 9 ' ' name) <> ": "
- <> colouredText' Vivid Green  (fmt $ padLeftF 2 ' ' count) <> tab <> boldCode <> "|" <> resetIntens
+ <> colouredText Dull Yellow (fmt $ padLeftF 9 ' ' name) <> ": "
+ <> colouredText Vivid Green  (fmt $ padLeftF 2 ' ' count) <> tab <> boldCode <> "|" <> resetIntens
  where tab = "\t" :: Text
 
-colouredText :: Show a => ColorIntensity -> Color -> a -> Text
-colouredText intensity colour = colouredText' intensity colour . pack . show
-
-colouredText' :: ColorIntensity -> Color -> Text -> Text
-colouredText' intensity colour text = format "{}{}{}" (setSGRCode [SetColor Foreground intensity colour]) text (setSGRCode [])
+colouredText :: ColorIntensity -> Color -> Text -> Text
+colouredText intensity colour text = format "{}{}{}" (setSGRCode [SetColor Foreground intensity colour]) text (setSGRCode [])
 
 boldCode, resetIntens, italCode, resetItal, underCode, resetUnder :: Text
 boldCode    = pack $ setSGRCode [ SetConsoleIntensity  BoldIntensity   ]
