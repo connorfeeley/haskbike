9af608beb419a3bea55322561610d7edc88fa4b8
Connor Feeley <git@cfeeley.org>
2023-10-09 09:00:00 -0400

    db: add diagnostic table to database definition

--- src-lib/Database/BikeShare.hs
+++ src-lib/Database/BikeShare.hs
@@ -14,9 +14,12 @@ module Database.BikeShare
      ( BikeshareDb (..)
      , module Database.BikeShare.Types
      , bikeshareDb
+     , bikeshareDiagnostics
      , bikeshareStationInformation
      , bikeshareStationStatus
      ) where
+
+import           Control.Lens
 
 import           Database.Beam
 import           Database.BikeShare.Types
@@ -26,6 +29,7 @@ import           Database.BikeShare.Types
 data BikeshareDb f where
   BikeshareDb :: { _bikeshareStationInformation :: f (TableEntity StationInformationT)
                  , _bikeshareStationStatus      :: f (TableEntity StationStatusT)
+                 , _bikeshareDiagnostics        :: f (TableEntity DiagnosticsT)
                  } -> BikeshareDb f
   deriving (Generic, Database be)
 
@@ -73,10 +77,19 @@ bikeshareDb = defaultDbSettings `withDbModification`
       , _d_status_vehicle_types_available = vehicleTypeFields "vehicle_types_available"
       , _d_status_active                  = "active"
       }
+  , _bikeshareDiagnostics =
+    setEntityName "diagnostics" <> modifyTableFields tableModification
+      { _diagnosticId   = "id"
+      , _diagnosticTime = "time"
+      }
   }
 
 -- Lenses
+bikeshareStationInformation     :: Lens' (BikeshareDb f) (f (TableEntity StationInformationT))
+bikeshareStationStatus          :: Lens' (BikeshareDb f) (f (TableEntity StationStatusT))
+bikeshareDiagnostics            :: Lens' (BikeshareDb f) (f (TableEntity DiagnosticsT))
 BikeshareDb
   (TableLens bikeshareStationInformation)
   (TableLens bikeshareStationStatus)
+  (TableLens bikeshareDiagnostics)
   = dbLenses
