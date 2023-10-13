f80146b36fe95070c63883a9ff7f90dcffc70922
Connor Feeley <git@cfeeley.org>
2023-10-13 09:00:00 -0400

    test: add failing test for when active status is missing

--- test/TestDatabase.hs
+++ test/TestDatabase.hs
@@ -16,6 +16,7 @@ module TestDatabase
      , unit_insertStationInformationApi
      , unit_insertStationStatus
      , unit_insertStationStatusApi
+     , unit_noActiveStationStatus
      , unit_queryDockingUndockingCount
      , unit_queryStationByIdAndName
      , unit_queryStationStatus
@@ -25,26 +26,31 @@ module TestDatabase
      , unit_separateNewerStatusRecordsInsertTwice
      ) where
 
-import           API.ResponseWrapper           ( response_data )
-import           API.Types                     ( StationInformationResponse, StationStatusResponse, _unInfoStations,
-                                                 status_station_id, unInfoStations, unStatusStations )
-import qualified API.Types                     as AT
+import           API.ResponseWrapper                      ( response_data )
+import           API.Types                                ( StationInformationResponse, StationStatusResponse,
+                                                            _unInfoStations, status_station_id, unInfoStations,
+                                                            unStatusStations )
+import qualified API.Types                                as AT
 
 import           AppEnv
 
 import           Control.Lens
 
-import           Data.Aeson                    ( FromJSON, eitherDecode )
-import qualified Data.ByteString.Lazy          as BL
-import           Data.Functor                  ( void )
-import           Data.Int                      ( Int32 )
+import           Data.Aeson                               ( FromJSON, eitherDecode )
+import qualified Data.ByteString.Lazy                     as BL
+import           Data.Functor                             ( void )
+import           Data.Int                                 ( Int32 )
 
+import           Database.Beam
+import           Database.Beam.Backend.SQL.BeamExtensions ( runUpdateReturningList )
 import           Database.Beam.Postgres
 import           Database.BikeShare
 import           Database.BikeShare.Operations
 import           Database.BikeShare.Utils
 
 import           Fmt
+
+import           Formatting
 
 import           ReportTime
 
@@ -470,3 +476,45 @@ checkConditions stationId thresholds expectDockings expectUndockings = do
 
 findInList :: Int32 -> [DockingEventsCount] -> Maybe DockingEventsCount
 findInList key tuples = tuples ^? folded . filtered (\k -> k ^. eventsStation . info_station_id == key)
+
+-- | HUnit test to check that station statuses are inserted correctly when there is no active status record.
+unit_noActiveStationStatus :: IO ()
+unit_noActiveStationStatus = do
+  setupTestDatabase
+  apiInfo     <- getDecodedFileInformation "docs/json/2.3/station_information-1.json"
+  apiStatus   <- getDecodedFileStatus      "docs/json/2.3/station_status-1.json"
+  apiStatus'  <- getDecodedFileStatus      "docs/json/2.3/station_status-2.json"
+
+  -- Insert test data.
+  _insertedInfo   <- runWithApp dbnameTest $ insertStationInformation $ apiInfo   ^. response_data . unInfoStations
+  _insertedStatus <- runWithApp dbnameTest $ insertStationStatus      $ apiStatus ^. response_data . unStatusStations
+
+  -- Disable the status for station 7003.
+  updated <- runWithApp dbnameTest $ withPostgres $ runUpdateReturningList $
+              update (bikeshareDb ^. bikeshareStationStatus)
+              (\c -> _d_status_active c <-. val_ False) -- Set active flag to False
+              (\c -> _d_status_info_id c ==. val_ (StationInformationId 7003) -- ... for station 7003.
+               &&. _d_status_active c ==. val_ True)
+  assertEqual "Deactivated status for station 7003" 1 (length updated)
+
+  pPrintCompact updated
+
+  -- Insert new status records.
+  updatedApiStatus' <- runWithApp dbnameTest $ separateNewerStatusRecords (apiStatus' ^. response_data . unStatusStations)
+  insertedStatus'   <- runWithApp dbnameTest $ insertStationStatus $ updatedApiStatus' ^. filter_newer
+
+  assertEqual "Inserted 302 newer status records"    302 (length $ insertedStatus' ^. insert_inserted)
+  -- 301 since we just manually deactivated the active status for station 7003.
+  assertEqual "Deactivated 301 older status records" 301 (length $ insertedStatus' ^. insert_deactivated)
+
+  -- Query active status for station 7003.
+  status <- runWithApp dbnameTest $ withPostgres $ runSelectReturningList $  select $ do
+    info <- all_ (bikeshareDb ^. bikeshareStationInformation)
+    guard_ (_info_station_id info ==. val_ 7003)
+    status <-  orderBy_ (asc_ . _d_status_station_id) $ all_ (bikeshareDb ^. bikeshareStationStatus)
+    guard_ (_d_status_info_id status `references_` info &&. _d_status_active status)
+    pure status
+
+  pPrintCompact status
+
+  assertEqual "Station 7003 has one active status" 1 (length status)
