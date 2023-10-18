65f7b00435bc9e4066bf2cb742d2e32353c2bb42
Connor Feeley <git@cfeeley.org>
2023-10-18 09:00:00 -0400

    emacs: disable lsp-lens

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((nil . ((lsp-nix-nil-formatter . ("nixfmt"))
          (eglot-workspace-configuration . (:haskell (:formattingProvider "stylish-haskell")))
-         (haskell-process-args-cabal-repl . ("--ghc-option=-ferror-spans")))))
+         (haskell-process-args-cabal-repl . ("--ghc-option=-ferror-spans"))
+         (lsp-lens-enable . nil))))
