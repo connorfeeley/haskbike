1ffddeba1975a72f4ef7d343eefdb59d9159b732
Connor Feeley <git@cfeeley.org>
2023-10-29 09:00:00 -0400

    server: clean up page structure

--- src-lib/Server/Page/IndexPage.hs
+++ src-lib/Server/Page/IndexPage.hs
@@ -30,12 +30,14 @@ instance ToHtml IndexPage where
       div_ [class_ "pure-u-1 pure-u-md-2-3"] $ do
         contentSubhead "About"
         p_ "I started collecting station-level data from Toronto's Bike Share API so that I could get a historical view of the system's usage. I've been collecting data about every station in the network roughly every 30s, since September 24th, 2023." <> br_ []
-        p_ "Fundamentally, the data is a time series of the number of bikes (broken down by the type of each bike) available at each station, as well as the number of docks available and disabled at each station." <> br_ []
+        p_ "The data available from Toronto's Bike Share API is, primarily:"
+        ul_ [class_ "pure-menu-list"] $ do
+          li_ "the number of available bikes (broken down by the type of each bike) at each station"
+          li_ $ "the number of disabled bikes (" <> i_ "not" <> "broken down by the type of each bike) at each station"
+          li_ "the number of docks available and disabled at each station"
         div_ [] $ do
           p_ $ "This project was borne out of my frustration for why " <> a_ [href_ "/visualization/station-status?station-id=7001&start-time=2023-09-24T00%3A00"] "Wellesley Station" <> " has had a complement of at least 9 - often more - disabled e-bikes ever since the station was converted to a charging station a few months ago."
           p_ [style_ "font-style: italic"] "Click the link to see the last 24 hours of data for Wellesley Station."
-          p_ [style_ "font-style: italic"] "Play around with the date ranges to "
-          p_ "http://localhost:8081/visualization/station-status?station-id=7001&start-time=2023-10-19T12%3A00&end-time=2023-10-20T18%3A00"
 
 
         contentSubhead "Source Code"
