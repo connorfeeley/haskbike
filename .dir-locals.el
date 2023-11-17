c7abcc1e747e0569d6143f91c4c1c94be128c679
Connor Feeley <git@cfeeley.org>
2023-11-17 09:00:00 -0500

    dir-locals: fix nix formatter

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
-((nil . ((lsp-nix-nil-formatter . ("nixfmt"))
+((nil . ((lsp-nix-nil-formatter . ("nixpkgs-fmt"))
          (eglot-workspace-configuration . (:haskell (:formattingProvider "stylish-haskell")))
          (haskell-process-args-cabal-repl . ("--ghc-option=-ferror-spans"))
          (lsp-lens-enable . nil))))
