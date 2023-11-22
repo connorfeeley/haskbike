750d7250abdba857d7820a937fe73dc0a7399e1c
Connor Feeley <git@cfeeley.org>
2023-11-22 09:00:00 -0500

    test(queryDockingsManual): optimize test

--- test/TestChargings.hs
+++ test/TestChargings.hs
@@ -123,67 +123,46 @@ unit_queryDockingsManual = do
   -- Check docking events.
   void $ do
     -- First E-Fit charged.
-    assertDockings   2 0 2 0 (between 0 5)
-    assertUndockings 1 0 1 0 (between 0 5)
+    assertBetween   (2, 0, 2, 0) (1, 0, 1, 0) (0, 5)
 
     -- Second two bikes (E-Fit and E-Fit G5) charged.
-    assertDockings   4 0 3 1 (between 0 8)
-    assertUndockings 2 0 2 0 (between 0 8)
+    assertBetween   (4, 0, 3, 1) (2, 0, 2, 0) (0, 8)
 
     -- Genuinely broken bike is docked.
-    assertDockings   4 0 3 1 (between 0 9)
-    assertUndockings 4 0 3 1 (between 0 9)
+    assertBetween   (4, 0, 3, 1) (4, 0, 3, 1) (0, 9)
 
-    assertDockings   4 0 3 1 (between 0 10)
-    assertUndockings 4 0 3 1 (between 0 10)
+    assertBetween   (4, 0, 3, 1) (4, 0, 3, 1) (0, 10)
 
     -- Dock a dead E-Fit G5, dock a charged E-Fit, undock the charged E-Fit, charge the initally docked E-Fit G5.
-    assertDockings   6 0 4 2 (between 0 14)
-    assertUndockings 5 0 4 1 (between 0 14)
+    assertBetween   (6, 0, 4, 2) (5, 0, 4, 1) (0, 14)
 
     -- Dock an Iconic.
-    assertDockings   7 1 4 2 (between 0 15)
-    assertUndockings 5 0 4 1 (between 0 15)
+    assertBetween   (7, 1, 4, 2) (5, 0, 4, 1) (0, 15)
 
   where
-    -- | Get docking events between two timestamps (with varying minute values).
-    between startMinute endMinute = runWithAppM dbnameTest $ queryDockingEventsCount (variation startMinute endMinute)
+    assertBetween
+      (expectedDockingAll, expectedDockingIconic, expectedDockingEfit, expectedDockingEfitG5)
+      (expectedUndockingAll, expectedUndockingIconic, expectedUndockingEfit, expectedUndockingEfitG5)
+      (startMinute, endMinute) = do
+      events <- runWithAppMDebug dbnameTest $ queryDockingEventsCount (variation startMinute endMinute)
 
-    -- | Assert that the number of docking events is as expected.
-    assertDockings expectedAll expectedIconic expectedEfit expectedEfitG5 cond = do
-      events <- cond
-      assertEqual "Expected total dockings"    expectedAll    (abs (sumEvents Docking (allBikeEvents events)))
-      assertEqual "Expected Iconic dockings"   expectedIconic (abs (sumEvents Docking (iconicEvents  events)))
-      assertEqual "Expected E-Fit dockings"    expectedEfit   (abs (sumEvents Docking (efitEvents    events)))
-      assertEqual "Expected E-Fit G5 dockings" expectedEfitG5 (abs (sumEvents Docking (efitG5Events  events)))
+      let minutesString = " at " ++ show startMinute ++ "-" ++ show endMinute
 
-    -- | Assert that the number of undocking events is as expected.
-    assertUndockings expectedAll expectedIconic expectedEfit expectedEfitG5 cond = do
-      events <- cond
-      assertEqual "Expected total undockings"    expectedAll    (abs (sumEvents Undocking (allBikeEvents events)))
-      assertEqual "Expected Iconic undockings"   expectedIconic (abs (sumEvents Undocking (iconicEvents  events)))
-      assertEqual "Expected E-Fit undockings"    expectedEfit   (abs (sumEvents Undocking (efitEvents    events)))
-      assertEqual "Expected E-Fit G5 undockings" expectedEfitG5 (abs (sumEvents Undocking (efitG5Events  events)))
+      assertEqual ("Expected total dockings"    ++ minutesString) expectedDockingAll    (abs (sumEvents Docking (allBikeEvents events)))
+      assertEqual ("Expected Iconic dockings"   ++ minutesString) expectedDockingIconic (abs (sumEvents Docking (iconicEvents  events)))
+      assertEqual ("Expected E-Fit dockings"    ++ minutesString) expectedDockingEfit   (abs (sumEvents Docking (efitEvents    events)))
+      assertEqual ("Expected E-Fit G5 dockings" ++ minutesString) expectedDockingEfitG5 (abs (sumEvents Docking (efitG5Events  events)))
 
-    -- Query charging events between two minutes, for all stations.
+      assertEqual ("Expected total undockings"    ++ minutesString) expectedUndockingAll    (abs (sumEvents Undocking (allBikeEvents events)))
+      assertEqual ("Expected Iconic undockings"   ++ minutesString) expectedUndockingIconic (abs (sumEvents Undocking (iconicEvents  events)))
+      assertEqual ("Expected E-Fit undockings"    ++ minutesString) expectedUndockingEfit   (abs (sumEvents Undocking (efitEvents    events)))
+      assertEqual ("Expected E-Fit G5 undockings" ++ minutesString) expectedUndockingEfitG5 (abs (sumEvents Undocking (efitG5Events  events)))
+
+    -- Query docking and undocking events between two minutes (timestamps), for all stations.
     variation startMinute endMinute = StatusVariationQuery Nothing
       [ EarliestTime (UTCTime (fromGregorian 2023 01 01) (timeOfDayToTime (TimeOfDay 0 startMinute 0)))
       , LatestTime   (UTCTime (fromGregorian 2023 01 01) (timeOfDayToTime (TimeOfDay 0 endMinute   0)))
       ]
-
--- | HUnit test to query all charging events (using exported database dump).
--- unit_queryDisabledBikeSeconds :: IO ()
--- unit_queryDisabledBikeSeconds = do
---   setupTestDatabase
---   initDBWithExportedData
-
---   assertChargings 135 41 94 (runWithAppM dbnameTest $ queryDisabledBikeSeconds variation)
---   where
---     -- Query for all stations, for all data in the test dataset.
---     variation = StatusVariationQuery (Just 7001)
---       [ EarliestTime (UTCTime (read "2023-01-01") (timeOfDayToTime midnight))
---       , LatestTime   (UTCTime (read "2024-01-01") (timeOfDayToTime midnight))
---       ]
 
 -- | HUnit test to query the number of {bikes, docks} {available, disabled} and {iconic, efit, efit g5} available across entire system at a point in time.
 unit_querySystemStatus :: IO ()
