fa9f95bf00536f838d746b1ceeb5effd714decf9
Connor Feeley <git@cfeeley.org>
2023-11-26 09:00:00 -0500

    server: add sleep-database endpoint

--- src-lib/Server/DebugAPI.hs
+++ src-lib/Server/DebugAPI.hs
@@ -8,10 +8,17 @@ module Server.DebugAPI
      , debugApiHandler
      ) where
 
+import           Colog
+
+import           Data.String                ( fromString )
+
 import           Database.Beam
+import           Database.PostgreSQL.Simple
+
+import           Fmt
 
 import           Servant
-import           Servant.Server.Generic ( AsServerT )
+import           Servant.Server.Generic     ( AsServerT )
 
 import           ServerEnv
 
@@ -23,15 +30,30 @@ data DebugAPI mode where
     { serverVersion :: mode :-
       "version"
         :> Get '[JSON] Version
+    , sleepDatabase :: mode :-
+      "sleep-database"
+        :> Capture "seconds" Int
+        :> Get '[JSON] ()
     } -> DebugAPI mode
   deriving stock Generic
 
 debugApiHandler :: DebugAPI (AsServerT ServerAppM)
 debugApiHandler =
   DebugAPI { serverVersion = versionHandler
+           , sleepDatabase = sleepDatabaseHandler
            }
 
 type Version = ((String, String), (String, String))
 
 versionHandler :: ServerAppM Version
 versionHandler = pure (("version", getCabalVersion), ("git-version", getGitVersion))
+
+sleepDatabaseHandler :: Int -> ServerAppM ()
+sleepDatabaseHandler seconds = do
+  env <- ask -- Get the ServerEnv within ServerAppM context
+
+  logInfo $ format "Sleeping database for {} seconds" seconds
+
+  let conn = envDBConnection (serverAppEnv env)
+  _ :: [Only ()] <- liftIO $ query_ conn $ fromString ("SELECT pg_sleep(" ++ show seconds ++ ")")
+  pure ()
