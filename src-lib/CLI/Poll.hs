14e3e52bf92e6ff32bf89abce23ff5a39300cfcd
Connor Feeley <git@cfeeley.org>
2023-11-29 09:00:00 -0500

    poll: add Utils module

--- src-lib/CLI/Poll.hs
+++ src-lib/CLI/Poll.hs
@@ -6,42 +6,33 @@ module CLI.Poll
 
 
 import           API.Client
-import           API.ClientLifted
 import           API.Pollable
 import           API.ResponseWrapper
-import qualified API.Types                     as AT
+import qualified API.Types                             as AT
 
 import           AppEnv
 
-import           CLI.Options                   ( PollOptions (..) )
+import           CLI.Options                           ( PollOptions (..) )
+import           CLI.Poll.Utils
 
-import           Colog                         ( log, logException, logInfo, pattern D, pattern I, pattern W )
+import           Colog                                 ( logDebug, logInfo )
 
-import           Control.Concurrent            ( threadDelay )
-import           Control.Concurrent.STM
-import           Control.Exception             ( throw )
 import           Control.Lens
-import           Control.Monad                 ( void, when )
-import           Control.Monad.Cont            ( forever )
+import           Control.Monad                         ( void )
 
-import           Data.Maybe                    ( fromMaybe, isJust, isNothing )
-import qualified Data.Text                     as Text
-import           Data.Time.Extras
+import qualified Data.Text                             as T
 
-import           Database.BikeShare
 import           Database.BikeShare.Operations
+import           Database.BikeShare.StationInformation
+import           Database.BikeShare.StationStatus
 
-import           Fmt
+import           Fmt                                   ( format )
 
-import           Formatting
+import           Formatting                            ( pShowCompact )
 
-import           Prelude                       hiding ( log )
+import           Prelude                               hiding ( log )
 
-import           Servant.Client                ( ClientM )
-
-import           TextShow                      ( showt )
-
-import           UnliftIO                      ( Async, async, liftIO, waitAnyCancel, waitBoth )
+import           UnliftIO
 
 
 -- | Dispatch CLI arguments to the poller.
@@ -88,79 +79,6 @@ pollClient = do
     logInfo "Polling and handling threads running. Press Ctrl+C to terminate."
     return ()
 
--- Create async actions for both requester and handler threads.
-createPollingThread :: Pollable a => TBQueue a -> TVar Int -> TVar Int -> AppM (Async b)
-createPollingThread queue ttlVar lastUpdatedVar = (async . forever) (requester queue ttlVar lastUpdatedVar)
-
-createHandlingThread :: Pollable a => TBQueue a -> AppM (Async b)
-createHandlingThread queue = async . forever $ handler queue
-
--- * TTL and timing handlers.
-
--- | Handle last_updated field in response.
-handleTimeElapsed :: Text.Text         -- ^ Log prefix
-                  -> ResponseWrapper a -- ^ TTL from API response.
-                  -> TVar Int          -- ^ TVar holding last_updated between threads.
-                  -> AppM (Maybe Int)     -- ^ Optional number of seconds extend TTL by, if last_updated went backwards.
-handleTimeElapsed logPrefix apiResult lastUpdatedVar = do
-  let currentTime' = apiResult ^. respLastUpdated
-  previousTime <- liftIO $ readTVarIO lastUpdatedVar
-  let previousTime' = posixToLocal previousTime
-  let timeElapsed = utcToPosix currentTime' - previousTime
-
-  -- Check if last_updated went backwards
-  if timeElapsed >= 0
-    then do -- Update last_updated variable.
-      liftIO $ atomically $ writeTVar lastUpdatedVar (utcToPosix currentTime')
-      log I $ format "({}) last updated [{}]" logPrefix currentTime'
-      pure Nothing
-    else do
-      log W $ format "({}) last updated went backwards: [{}] -> [{}] | ({})" logPrefix previousTime' currentTime' timeElapsed
-      pure (Just (-timeElapsed))
-
-
--- | Handle last_updated field in response.
-handleTTL :: Pollable (ResponseWrapper a)
-          => Text.Text         -- ^ Log prefix
-          -> ResponseWrapper a -- ^ TTL from API response.
-          -> TVar Int          -- ^ TVar holding TTL.
-          -> Maybe Int         -- ^ Optional number of seconds to extend TTL by.
-          -> AppM ()
-handleTTL logPrefix apiResult ttlVar extendBy = do
-  let ttlSecs = apiResult ^. respTtl
-
-  when (isJust extendBy) $ log W $ format "({}) extending TTL by {}s" logPrefix (fromMaybe 0 extendBy)
-  log I $ format "({}) TTL={}{}" logPrefix (showt ttlSecs) (maybe "" (("+" ++) . show) extendBy)
-
-  liftIO $ atomically $ writeTVar ttlVar (ttlSecs + fromMaybe 0 extendBy)
-
-requesterFn :: Pollable (ResponseWrapper a)
-            => Text.Text
-            -> ClientM (ResponseWrapper a)
-            -> TBQueue (ResponseWrapper a)
-            -> TVar Int
-            -> TVar Int
-            -> AppM ()
-requesterFn prefix query queue intervalSecsVar lastUpdated = void $ do
-  runQueryM query >>= \case
-    Left err -> logException err >> throw err
-    Right result -> do
-      -- Handle TTL upfront, so that we don't sleep when called
-      -- for the first time from 'pollClient' (TTL = 0).
-      intervalSecs <- liftIO $ readTVarIO intervalSecsVar
-      log D $ format "({}) Sleeping for {} seconds." prefix intervalSecs
-      liftIO $ threadDelay (intervalSecs * 1000000)
-
-      elapsedResult <- handleTimeElapsed prefix result lastUpdated
-      handleTTL prefix result intervalSecsVar elapsedResult
-
-      -- If 'elapsedResult' is a 'Just', that indicates that last_updated went backwards.
-      -- In that case, we should NOT write the result to the queue, and instead extend the
-      -- time until our next poll by however long the time went backwards.
-      when (isNothing elapsedResult) (liftIO $ atomically $ writeTBQueue queue result)
-
--- * Station status handling.
-
 instance Pollable (ResponseWrapper [AT.StationStatus]) where
   -- | Endpoint (ClientM a)
   request = stationStatus
@@ -173,16 +91,16 @@ instance Pollable (ResponseWrapper [AT.StationStatus]) where
     response <- liftIO $ atomically $ readTBQueue queue
 
     let status = response ^. respData
-    log I $ format "(Status) Received {} status records from API." (length status)
+    logInfo $ format "(Status) Received {} status records from API." (length status)
 
     -- Insert the updated status.
     insertedResult <- insertStationStatus status
 
     -- Log each station ID updated.
-    mapM_ (log D . Text.pack . fmtLog . dataToTuple) insertedResult
+    mapM_ (logDebug . T.pack . fmtLog . dataToTuple) insertedResult
 
     -- Log number of inserted rows.
-    log I $ format "(Status) Inserted: {}" (length insertedResult)
+    logInfo $ format "(Status) Inserted: {}" (length insertedResult)
 
     where
       dataToTuple s = ( s ^. statusStationId . unInformationStationId
@@ -205,16 +123,16 @@ instance Pollable (ResponseWrapper [AT.StationInformation]) where
     response <- liftIO $ atomically $ readTBQueue queue
 
     let info = response ^. respData
-    log I $ format "(Info) Received {} info records from API." (length info)
+    logInfo $ format "(Info) Received {} info records from API." (length info)
 
     -- Insert the station information (updating existing records, if existing).
     insertedResult <- insertStationInformation info
 
     -- Log each station ID updated.
-    mapM_ (log D . Text.pack . fmtLog) insertedResult
+    mapM_ (logDebug . T.pack . fmtLog) insertedResult
 
     -- Log number of inserted rows.
-    log I $ format "(Info) Updated/inserted {} records into database." (length insertedResult)
+    logInfo $ format "(Info) Updated/inserted {} records into database." (length insertedResult)
 
     where
       fmtLog inserted = format "ID: {}" (pShowCompact inserted)
@@ -233,7 +151,7 @@ instance Pollable (ResponseWrapper AT.SystemInformation) where
     response <- liftIO $ atomically $ readTBQueue queue
 
     let (reported, info) = (response ^. respLastUpdated, response ^. respData)
-    log I "(Sys Info) Received system info from API."
+    logInfo "(Sys Info) Received system info from API."
 
     -- Insert the station information (updating existing records, if existing).
     (_insertedInfo, _insertedInfoCount) <- insertSystemInformation reported info
---
+++ src-lib/CLI/Poll/Utils.hs
@@ -0,0 +1,105 @@
+-- |
+
+module CLI.Poll.Utils
+     ( createHandlingThread
+     , createPollingThread
+     , handleTTL
+     , handleTimeElapsed
+     , requesterFn
+     ) where
+import           API.ClientLifted    ( runQueryM )
+import           API.Pollable
+import           API.ResponseWrapper
+
+import           AppEnv
+
+import           Colog
+
+import           Control.Concurrent
+import           Control.Exception   ( throw )
+import           Control.Lens
+import           Control.Monad
+
+import           Data.Maybe          ( fromMaybe, isJust, isNothing )
+import qualified Data.Text           as T
+import           Data.Time.Extras
+
+import           Fmt                 ( format )
+
+import           Servant.Client
+
+import           TextShow            ( showt )
+
+import           UnliftIO
+
+
+-- Create async actions for both requester and handler threads.
+createPollingThread :: Pollable a => TBQueue a -> TVar Int -> TVar Int -> AppM (Async b)
+createPollingThread queue ttlVar lastUpdatedVar = (async . forever) (requester queue ttlVar lastUpdatedVar)
+
+createHandlingThread :: Pollable a => TBQueue a -> AppM (Async b)
+createHandlingThread queue = async . forever $ handler queue
+
+-- * TTL and timing handlers.
+
+-- | Handle last_updated field in response.
+handleTimeElapsed :: T.Text            -- ^ Log prefix
+                  -> ResponseWrapper a -- ^ TTL from API response.
+                  -> TVar Int          -- ^ TVar holding last_updated between threads.
+                  -> AppM (Maybe Int)  -- ^ Optional number of seconds extend TTL by, if last_updated went backwards.
+handleTimeElapsed logPrefix apiResult lastUpdatedVar = do
+  let currentTime' = apiResult ^. respLastUpdated
+  previousTime <- liftIO $ readTVarIO lastUpdatedVar
+  let previousTime' = posixToLocal previousTime
+  let timeElapsed = utcToPosix currentTime' - previousTime
+
+  -- Check if last_updated went backwards
+  if timeElapsed >= 0
+    then do -- Update last_updated variable.
+      liftIO $ atomically $ writeTVar lastUpdatedVar (utcToPosix currentTime')
+      logWarning $ format "({}) last updated [{}]" logPrefix currentTime'
+      pure Nothing
+    else do
+      logWarning $ format "({}) last updated went backwards: [{}] -> [{}] | ({})" logPrefix previousTime' currentTime' timeElapsed
+      pure (Just (-timeElapsed))
+
+
+-- | Handle last_updated field in response.
+handleTTL :: Pollable (ResponseWrapper a)
+          => T.Text            -- ^ Log prefix
+          -> ResponseWrapper a -- ^ TTL from API response.
+          -> TVar Int          -- ^ TVar holding TTL.
+          -> Maybe Int         -- ^ Optional number of seconds to extend TTL by.
+          -> AppM ()
+handleTTL logPrefix apiResult ttlVar extendBy = do
+  let ttlSecs = apiResult ^. respTtl
+
+  when (isJust extendBy) $ logWarning $ format "({}) extending TTL by {}s" logPrefix (fromMaybe 0 extendBy)
+  logInfo $ format "({}) TTL={}{}" logPrefix (showt ttlSecs) (maybe "" (("+" ++) . show) extendBy)
+
+  liftIO $ atomically $ writeTVar ttlVar (ttlSecs + fromMaybe 0 extendBy)
+
+requesterFn :: Pollable (ResponseWrapper a)
+            => T.Text
+            -> ClientM (ResponseWrapper a)
+            -> TBQueue (ResponseWrapper a)
+            -> TVar Int
+            -> TVar Int
+            -> AppM ()
+requesterFn prefix query queue intervalSecsVar lastUpdated = void $ do
+  runQueryM query >>= \case
+    Left err -> logException err >> throw err
+    Right result -> do
+      -- Handle TTL upfront, so that we don't sleep when called
+      -- for the first time from 'pollClient' (TTL = 0).
+      intervalSecs <- liftIO $ readTVarIO intervalSecsVar
+      logDebug $ format "({}) Sleeping for {} seconds." prefix intervalSecs
+      liftIO $ threadDelay (intervalSecs * 1000000)
+
+      elapsedResult <- handleTimeElapsed prefix result lastUpdated
+      handleTTL prefix result intervalSecsVar elapsedResult
+
+      -- If 'elapsedResult' is a 'Just', that indicates that last_updated went backwards.
+      -- In that case, we should NOT write the result to the queue, and instead extend the
+      -- time until our next poll by however long the time went backwards.
+      when (isNothing elapsedResult) (liftIO $ atomically $ writeTBQueue queue result)
