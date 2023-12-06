cd54d56937131f9df22f418396a7114b4c2eb9df
Connor Feeley <git@cfeeley.org>
2023-12-06 09:00:00 -0500

    db: add QueryLog table

---
+++ src-lib/Database/BikeShare/QueryLogs.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE DeriveAnyClass            #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TypeFamilies              #-}
+
+-- | Table to hold logs of queries to the API.
+
+module Database.BikeShare.QueryLogs
+     ( PrimaryKey (QueryLogId)
+     , QueryLog
+     , QueryLogId
+     , QueryLogT (..)
+       -- Lenses
+     , queryLogEndpoint
+     , queryLogId
+     , queryLogTime
+     ) where
+
+import           Control.Lens
+
+import           Data.Int
+import           Data.Time
+
+import           Database.Beam
+import           Database.Beam.Backend ( SqlSerial )
+
+
+data EndpointQueried = StationInformation
+                     | StationStatus
+                     | SystemInformation
+                     deriving (Generic, Show, Eq)
+
+data QueryLogT f where
+  QueryLog :: { _queryLogId       :: Columnar f (SqlSerial Int32)
+              , _queryLogTime     :: Columnar f UTCTime
+              , _queryLogEndpoint :: Columnar f EndpointQueried
+              } -> QueryLogT f
+  deriving (Generic, Beamable)
+
+type QueryLog = QueryLogT Identity
+type QueryLogId = PrimaryKey QueryLogT Identity
+
+deriving instance Show QueryLogId
+deriving instance Eq QueryLogId
+deriving instance Show QueryLog
+deriving instance Eq QueryLog
+
+instance Table QueryLogT where
+  data PrimaryKey QueryLogT f = QueryLogId (C f (SqlSerial Int32))
+    deriving (Generic, Beamable)
+  primaryKey = QueryLogId . _queryLogId
+
+queryLogId :: Lens' (QueryLogT f) (C f (SqlSerial Int32))
+queryLogTime :: Lens' (QueryLogT f) (C f UTCTime)
+queryLogEndpoint :: Lens' (QueryLogT f) (C f EndpointQueried)
+QueryLog (LensFor queryLogId) _ _ = tableLenses
+QueryLog _ (LensFor queryLogTime) _ = tableLenses
+QueryLog _ _ (LensFor queryLogEndpoint) = tableLenses
