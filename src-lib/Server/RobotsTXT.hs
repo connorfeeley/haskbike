1d921e48232ac48104d739e34e400a4da53f9e94
Connor Feeley <git@cfeeley.org>
2023-12-14 09:00:00 -0500

    server(robots): deny debug, data endpoints

--- src-lib/Server/RobotsTXT.hs
+++ src-lib/Server/RobotsTXT.hs
@@ -5,11 +5,15 @@
 
 module Server.RobotsTXT
      ( RobotsAPI (..)
+     , RobotsDefault (..)
+     , RobotsUA (..)
+     , makeRobots
+     , packRobots
      , robotsHandler
      , robotsTxtHandler
      ) where
 import qualified Data.ByteString.Char8  as BSW
-import qualified Data.Text              as T
+import           Data.Maybe             ( catMaybes )
 
 import           GHC.Generics           ( Generic )
 
@@ -17,6 +21,8 @@ import           Servant
 import           Servant.Server.Generic
 
 import           ServerEnv
+
+-- * API handler.
 
 data RobotsAPI mode where
   RobotsAPI ::
@@ -28,4 +34,57 @@ robotsHandler :: RobotsAPI (AsServerT ServerAppM)
 robotsHandler =  RobotsAPI robotsTxtHandler
 
 robotsTxtHandler :: ServerAppM BSW.ByteString
-robotsTxtHandler = pure $ BSW.pack "TEST"
+robotsTxtHandler = pure $
+  packRobots $ makeRobots AllUA AllAllowed (Just 1) ["debug", "data"]
+
+
+-- * Types and builders.
+
+data RobotsDefault where
+  AllAllowed    :: RobotsDefault
+  AllDisallowed :: RobotsDefault
+
+data RobotsUA where
+  AllUA   :: RobotsUA
+  RobotUA :: String -> RobotsUA
+
+{- Make a simple robots.txt.
+  >>> makeRobots AllUA AllAllowed (Just 1) ["debug", "data"]
+-}
+makeRobots :: RobotsUA -> RobotsDefault -> Maybe Int -> [String] -> [String]
+makeRobots userAgent robotsDefault crawlDelay disallowRoutes = catMaybes
+  [ (Just . userAgentTxt) userAgent
+  , (Just . defaultDisallowTxt) robotsDefault
+  , crawlDelayTxt crawlDelay
+  ] ++ map disallowTxt disallowRoutes
+
+-- | Pack the lines from 'makeRobots' into an ASCII 'ByteString'.
+packRobots :: [String] -> BSW.ByteString
+packRobots = BSW.pack . unlines
+
+userAgentTxt :: RobotsUA -> String
+userAgentTxt ua = "User-agent: " <> case ua of
+  AllUA           -> "*"
+  RobotUA robotUA -> robotUA
+
+defaultDisallowTxt :: RobotsDefault -> String
+defaultDisallowTxt robotsDefault = "Disallow: " <> case robotsDefault of
+  AllAllowed    -> ""
+  AllDisallowed -> "/"
+
+disallowTxt :: String -> String
+disallowTxt path = "Disallow: /" <> path <> "/"
+
+crawlDelayTxt :: Maybe Int -> Maybe String
+crawlDelayTxt = fmap (("Crawl-delay: " <>) . show )
+
+excludedRoutes' :: [String]
+excludedRoutes' = [ "User-agent: *"
+                 , "Disallow: "
+                 , "Crawl-delay: 1"
+                 , "Disallow: /debug/"
+                 , "Disallow: /data/"
+                 , "Disallow: /visualization/"
+                 , "Disallow: /components/"
+                 , "Disallow: /static/"
+                 ]
