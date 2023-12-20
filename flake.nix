54fe5ed38b967d993c23f45b0a0c360374cd4a33
Connor Feeley <git@cfeeley.org>
2023-12-20 09:00:00 -0500

    nix: track nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
 
