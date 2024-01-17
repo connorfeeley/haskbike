7a7869855780b8860e461ff026a5f5887239652c
Connor Feeley <git@cfeeley.org>
2024-01-17 09:00:00 -0500

    test: syntax error in unused file

--- test/Test.hs
+++ test/Test.hs
@@ -19,9 +19,10 @@ tree =
   askOption @NoRunNetworkOpt $ \case
     NoRunNetworkOpt Unset ->
       testGroup "group1"
-        if unsafePerformIO (putStrLn "unsafe things go here" >> pure True)
-          then [...]
-          else []
+        -- if unsafePerformIO (putStrLn "unsafe things go here" >> pure True)
+        --   then [...]
+        --   else []
+      []
     NoRunNetworkOpt Set ->
       -- Can't run IO here!
       testGroup "group1" []
