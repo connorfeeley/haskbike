7ed28a6700a3963cee8216e9b40d5c2be85ea2fb
Connor Feeley <git@cfeeley.org>
2024-03-12 09:00:00 -0400

    cli: add option for populating status changes table

--- src-lib/CLI/Options.hs
+++ src-lib/CLI/Options.hs
@@ -3,6 +3,10 @@
 -- | Command-line options.
 module CLI.Options where
 
+import qualified Data.Attoparsec.Text     as A
+import           Data.Either              ( fromRight )
+import           Data.Functor             ( ($>) )
+import qualified Data.Text                as T
 import           Data.Time
 
 import           Database.BikeShare.Utils
@@ -179,12 +183,49 @@ parseStationName = exact <|> prefix <|> suffix <|> wildcard where
 
 -- | Options for the 'Poll' command.
 data PollOptions where
-  PollOptions :: { } -> PollOptions
+  PollOptions :: { optPollPopulateStatusChanges :: !PopulateStatusChangesOpt
+                 } -> PollOptions
   deriving (Show)
 
--- | Parser for 'ResetOptions'.
+-- | Parser for 'PollOptions'.
 pollOptionsParser :: Parser PollOptions
-pollOptionsParser = pure PollOptions
+pollOptionsParser = PollOptions <$> populateStatusChangesParser
+
+-- | Valid options for populating the 'station status changes' table.
+data PopulateStatusChangesOpt where
+  AlwaysPopulate :: PopulateStatusChangesOpt
+  AutoPopulate   :: PopulateStatusChangesOpt
+  NeverPopulate  :: PopulateStatusChangesOpt
+  deriving (Enum, Bounded, Eq)
+
+instance Show PopulateStatusChangesOpt where
+  show AlwaysPopulate = "always"
+  show AutoPopulate   = "auto"
+  show NeverPopulate  = "never"
+
+-- | Read instance for 'PopulateStatusChangesOpt'.
+instance Read PopulateStatusChangesOpt where
+  readsPrec _ = fromRight [] . A.parseOnly parser . T.pack
+    where
+    parser :: A.Parser [(PopulateStatusChangesOpt, String)]
+    parser = A.choice
+      [ A.asciiCI "always" $> [(AlwaysPopulate, "")]
+      , A.asciiCI "auto"   $> [(AutoPopulate,   "")]
+      , A.asciiCI "never"  $> [(NeverPopulate,  "")]
+      ]
+
+-- | optparse-applicative 'Parser' instance for 'PopulateStatusChangesOpt'.
+populateStatusChangesParser :: Parser PopulateStatusChangesOpt
+populateStatusChangesParser = -- Relies on 'Read' instance.
+  option auto
+    ( long "populate-status-changes"
+   <> help ("Populate the 'station status changes' table with data from the 'station status' table. Allowed values: " <> show allowedValues)
+   <> value AutoPopulate
+   <> showDefault
+    )
+  where
+    allowedValues :: [PopulateStatusChangesOpt]
+    allowedValues = [minBound..]
 
 data ServeVisualizeOptions where
   ServeVisualizeOptions :: { optServeVisualizePort :: Int
