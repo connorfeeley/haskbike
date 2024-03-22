2e9324b467bc2651dd9bdad9018842bdc099f223
Connor Feeley <git@cfeeley.org>
2024-03-22 09:00:00 -0400

    client(test): assert response is successfully decoded

--- haskbike-client/test/TestClient.hs
+++ haskbike-client/test/TestClient.hs
@@ -4,11 +4,21 @@ module TestClient where
 import           Control.Exception   ( SomeException, try )
 import           Control.Monad       ( void )
 
+import           Data.Either         ( isRight )
+
 import           Haskbike.API.Client
 
 import           Prelude             hiding ( log, unwords )
 
 import           Test.Tasty.HUnit
+
+
+{-|
+Check that the result of decoding the query response was successful.
+This has the side-effect of forcing the evaluation of the response.
+-}
+checkResponse :: Either a b -> Assertion
+checkResponse = assertBool "Response decoding failed" . isRight
 
 
 -- | Mark a test as expected to fail.
@@ -19,23 +29,14 @@ markAsExpectedFailure testFunc = do
     Left _  -> return ()
     Right _ -> assertFailure "Expected failure, but test passed"
 
-unit_parseVersions           :: IO ()
-unit_parseVersions           = void $ runQueryWithEnv versions
 
-unit_parseVehicleTypes       :: IO ()
-unit_parseVehicleTypes       = void $ runQueryWithEnv vehicleTypes
-
-unit_parseStationInformation :: IO ()
-unit_parseStationInformation = void $ runQueryWithEnv stationInformation
-
-unit_parseStationStatus      :: IO ()
-unit_parseStationStatus      = void $ runQueryWithEnv stationStatus
-
-unit_parseSystemInformation  :: IO ()
-unit_parseSystemInformation  = void $ runQueryWithEnv systemInformation
-
-unit_parseSystemRegions      :: IO ()
-unit_parseSystemRegions      = void $ runQueryWithEnv systemRegions
-
-unit_parseSystemPricingPlans :: IO ()
-unit_parseSystemPricingPlans = void $ runQueryWithEnv systemPricingPlans
+-- | Test the client functions.
+unit_parseVersions, unit_parseVehicleTypes, unit_parseStationInformation, unit_parseStationStatus :: IO ()
+unit_parseSystemInformation, unit_parseSystemRegions, unit_parseSystemPricingPlans :: IO ()
+unit_parseVersions           = void . checkResponse =<< runQueryWithEnv versions
+unit_parseVehicleTypes       = void . checkResponse =<< runQueryWithEnv vehicleTypes
+unit_parseStationInformation = void . checkResponse =<< runQueryWithEnv stationInformation
+unit_parseStationStatus      = void . checkResponse =<< runQueryWithEnv stationStatus
+unit_parseSystemInformation  = void . checkResponse =<< runQueryWithEnv systemInformation
+unit_parseSystemRegions      = void . checkResponse =<< runQueryWithEnv systemRegions
+unit_parseSystemPricingPlans = void . checkResponse =<< runQueryWithEnv systemPricingPlans
