1a4c5b3c4a4ff236162d03880a619e78b663cdaa
Connor Feeley <git@cfeeley.org>
2024-03-29 09:00:00 -0400

    server(list): consolidate instances

--- haskbike-server/Haskbike/Server/Page/List/StationList.hs
+++ haskbike-server/Haskbike/Server/Page/List/StationList.hs
@@ -1,7 +1,8 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 {-# OPTIONS_GHC -Wno-unused-local-binds #-}
-{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingStrategies   #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 -- | This module defines the data types used to render the station status visualization page.
 
@@ -27,6 +28,10 @@ import           Prelude                                     hiding ( null )
 import           Servant
 
 
+-- - ---------------------------------------------------------------------------
+-- * Common type, type class, and instances.
+
+-- | Parametric station list type.
 data StationList a where
   StationList :: { _stationList           :: a
                  , _stationTimeRange      :: (Maybe LocalTime, Maybe LocalTime)
@@ -36,41 +41,7 @@ data StationList a where
                  } -> StationList a
 
 
-instance ToHtml (StationList [(StationInformation, StationStatus)]) where
-  toHtmlRaw = toHtml
-  toHtml params = do
-    div_ [class_ "header"] $ do
-      h1_ [] (toHtml "Station List")
-    div_ [class_ "content"] $ do
-      toHtml (StationListForm (_stationListInputs params))
-      toHtml (toStationListTable params)
-
-instance HasStationListPage [(StationInformation, StationStatus)] =>
-         ToHtmlComponents (StationList [(StationInformation, StationStatus)]) where
-  pageAnchor _ = "#station-list"
-  pageName   _ = "Station List"
-  toHead       = pageHead
-
-
--- | Table displaying station info, status, etc.
-toStationListTable :: Monad m => StationList a -> HtmlT m ()
-toStationListTable _ = do
-  div_ [id_ "station-list-table-too-small", style_ "display: none"]
-    "Screen too small to display table. Rotate your device or resize your browser window."
-  div_ [id_ "station-list-table", class_ "station-list-table"] mempty
-
-
--- * Station empty/full list.
-
-instance ToHtml (StationList [(StationInformation, StationStatus, EmptyFull)]) where
-  toHtmlRaw = toHtml
-  toHtml params = do
-    div_ [class_ "header"] $ do
-      h1_ [] (toHtml "Station Occupancy")
-    div_ [class_ "content"] $ do
-      toHtml (StationListForm (_stationListInputs params))
-      toHtml (toStationListTable params)
-
+-- | Type class for pages that display a list of stations.
 class HasStationListPage a where
   pageScript :: Monad m => StationList a -> HtmlT m ()
 
@@ -84,8 +55,42 @@ class HasStationListPage a where
     script_ [src_ ("/" <> toUrlPiece (_staticLink page) <> "/js/station-list-table.js"), defer_ mempty] ""
     pageScript page
 
+
+-- | Define instance used by both regular station list and station occupancy list.
+instance ToHtmlComponents (StationList a) => ToHtml (StationList a) where
+  toHtmlRaw = toHtml
+  toHtml params = do
+    div_ [class_ "header"] $ do
+      h1_ [] (toHtml (pageTitle params))
+    div_ [class_ "content"] $ do
+      toHtml (StationListForm (_stationListInputs params))
+      toHtml (toStationListTable params)
+
+
+-- | Table displaying station info, status, etc. Rendered using GridJS.
+toStationListTable :: (ToHtmlComponents (StationList a), Monad m)
+                   => StationList a -> HtmlT m ()
+toStationListTable _ = do
+  div_ [id_ "station-list-table-too-small", style_ "display: none"]
+    "Screen too small to display table. Rotate your device or resize your browser window."
+  div_ [id_ "station-list-table", class_ "station-list-table"] mempty
+
+
+-- - ---------------------------------------------------------------------------
+-- * Regular station list instances.
+
 instance HasStationListPage [(StationInformation, StationStatus)] where
   pageScript page = script_ [src_ ("/" <> toUrlPiece (_staticLink page) <> "/js/station-list.js"), defer_ mempty] ""
+
+instance HasStationListPage [(StationInformation, StationStatus)] =>
+         ToHtmlComponents (StationList [(StationInformation, StationStatus)]) where
+  pageAnchor _ = "#station-list"
+  pageName   _ = "Station List"
+  toHead       = pageHead
+
+
+-- - ---------------------------------------------------------------------------
+-- * Station occupancy list instances.
 
 instance HasStationListPage [(StationInformation, StationStatus, EmptyFull)] where
   pageScript page = script_ [src_ ("/" <> toUrlPiece (_staticLink page) <> "/js/station-occupancy.js"), defer_ mempty] ""
@@ -94,4 +99,5 @@ instance HasStationListPage [(StationInformation, StationStatus, EmptyFull)] =>
          ToHtmlComponents (StationList [(StationInformation, StationStatus, EmptyFull)]) where
   pageAnchor _ = "#station-occupancy"
   pageName   _ = "Station Occupancy"
+  pageTitle  _ = "Station List with Occupancy Time"
   toHead       = pageHead
