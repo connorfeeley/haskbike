919b66b179174c2ed45606af7f82730a6930b362
Connor Feeley <git@cfeeley.org>
2024-03-31 09:00:00 -0400

    db(occ): add function to cache occupancy

--- haskbike-database/Haskbike/Database/Operations/StationOccupancy.hs
+++ haskbike-database/Haskbike/Database/Operations/StationOccupancy.hs
@@ -1,8 +1,8 @@
 -- | Database queries and expressions for station occupancy.
 
 module Haskbike.Database.Operations.StationOccupancy
-     ( stationOccupancyE
-     , toStationOccupancy
+     ( cacheStationOccupancy
+     , stationOccupancyE
      ) where
 
 import           Control.Lens                                hiding ( reuse, (<.) )
@@ -11,7 +11,7 @@ import           Data.Int                                    ( Int32 )
 import           Data.Time
 
 import           Database.Beam
-import           Database.Beam.Backend                       ( BeamSqlBackend )
+import           Database.Beam.Backend.SQL.BeamExtensions    ( MonadBeamInsertReturning (runInsertReturningList) )
 import           Database.Beam.Postgres                      ( Postgres )
 import qualified Database.Beam.Postgres                      as Pg
 
@@ -22,22 +22,31 @@ import           Haskbike.Database.Tables.StationOccupancy
 import           Haskbike.Database.Tables.StationStatus
 
 -- | PostgreSQL GREATEST and LEAST.
-greatest_, least_ :: QGenExpr ctxt Postgres s a -> QGenExpr ctxt Postgres s a -> QGenExpr ctxt Postgres s a
+greatest_, least_ :: QGenExpr ctx Postgres s a -> QGenExpr ctx Postgres s a -> QGenExpr ctx Postgres s a
 greatest_ = customExpr_ (\a b -> "GREATEST(" <> a <> ", " <> b <> ")")
 least_    = customExpr_ (\a b -> "LEAST("    <> a <> ", " <> b <> ")")
 
 
 -- | Difference between two epochs.
-timeDelta :: (HasSqlTime tgt1, HasSqlTime tgt2, Integral b) => QGenExpr ctxt1 Postgres s tgt1 -> QGenExpr ctxt2 Postgres s tgt2 -> QGenExpr ctxt3 Postgres s b
+timeDelta :: (HasSqlTime t, Integral b)
+          => QGenExpr ctx Postgres s t -> QGenExpr ctx Postgres s t -> QGenExpr ctx Postgres s b
 timeDelta a b = cast_ (extract_ Pg.epoch_ a - extract_ Pg.epoch_ b) int
 
 -- | Query how long each station has been both empty and full for.
-stationOccupancyE :: (Integral a)
+stationOccupancyE :: Integral a
                   => Maybe a -> UTCTime -> UTCTime
                   -> Q Postgres BikeshareDb s ( StationInformationT (QGenExpr QValueContext Postgres s)
                                               , (QGenExpr QValueContext Postgres s (Maybe Int32), QGenExpr QValueContext Postgres s (Maybe Int32))
                                               )
-stationOccupancyE stationId startTime endTime = do
+stationOccupancyE = stationOccupancyThreshE 0 0
+
+-- | Query how long each station has been both empty and full for, with variable thresholds for being empty or full.
+stationOccupancyThreshE :: Integral a
+                        => Int32 -> Int32 -> Maybe a -> UTCTime -> UTCTime
+                        -> Q Postgres BikeshareDb s ( StationInformationT (QGenExpr QValueContext Postgres s)
+                                                    , (QGenExpr QValueContext Postgres s (Maybe Int32), QGenExpr QValueContext Postgres s (Maybe Int32))
+                                                    )
+stationOccupancyThreshE emptyThresh fullThresh stationId startTime endTime = do
   let statusCount = aggregate_ (\row -> ( group_ ((_unInformationStationId . _statusInfoId . _statusCommon) row)
                                         , as_ @Int32 countAll_
                                         )
@@ -57,15 +66,15 @@ stationOccupancyE stationId startTime endTime = do
                           period_end = least_ nReported (val_ endTime)
                        in ( group_ ((_unInformationStationId . _statusInfoId . _statusCommon) row)
                           -- 0 bikes available means the station is empty
-                          , fromMaybe_ 0 (sum_ (ifThenElse_ (row ^. statusNumBikesAvailable ==. val_ 0)
+                          , fromMaybe_ 0 (sum_ (ifThenElse_ (row ^. statusNumBikesAvailable ==. val_ emptyThresh)
                                                  (timeDelta period_end period_start) 0
                                                ) `filterWhere_` (period_start <. period_end))
                           -- 0 docks available means the station is full
-                          , fromMaybe_ 0 (sum_ (ifThenElse_ (row ^. statusNumDocksAvailable ==. val_ 0)
+                          , fromMaybe_ 0 (sum_ (ifThenElse_ (row ^. statusNumDocksAvailable ==. val_ fullThresh)
                                                  (timeDelta period_end period_start) 0
                                                ) `filterWhere_` (period_start <. period_end))
                           )) $
-          filter_ (\(row, lead, lag) -> keepRow row lead lag) $
+          filter_ (\(row, lead, lag) -> keepRow emptyThresh fullThresh row lead lag) $
           emptyFullRows stationId startTime endTime
   do
     (info, _statusCount') <- do
@@ -86,7 +95,8 @@ stationOccupancyE stationId startTime endTime = do
     pure (info, (empty, full))
 
 
-emptyFullRows :: Integral a => Maybe a -> UTCTime -> UTCTime
+emptyFullRows :: Integral a
+              => Maybe a -> UTCTime -> UTCTime
               -> Q Postgres BikeshareDb s (StationStatusT (QGenExpr QValueContext Postgres s), (QGenExpr QValueContext Postgres s UTCTime, QGenExpr QValueContext Postgres s Int32, QGenExpr QValueContext Postgres s Int32), (QGenExpr QValueContext Postgres s UTCTime, QGenExpr QValueContext Postgres s Int32, QGenExpr QValueContext Postgres s Int32))
 emptyFullRows stationId' startTime' endTime' =
   filter_ (\(row, (lead, _, _), (lag, _, _)) -> (row  ^. statusLastReported) <. val_ endTime' &&.
@@ -109,63 +119,62 @@ emptyFullRows stationId' startTime' endTime' =
         filter_ (\row -> between_ (row ^. statusLastReported) (val_ (addUTCTime (-12 * 60 * 60) startTime')) (val_ (addUTCTime (1 * 60 * 60) endTime'))) $
         all_ (bikeshareDb ^. bikeshareStationStatus)
 
-keepRow :: ( BeamSqlBackend be, SqlValable b1, SqlValable b2, SqlValable a1,  SqlValable a2
-           , SqlEq (QGenExpr context be s) b1,  SqlEq (QGenExpr context be s) b2, SqlEq (QGenExpr context be s) a1,  SqlEq (QGenExpr context be s) a2
-           , SqlEq (QGenExpr context be s) (Columnar f Int32)
-           , Num (Columnar f Int32), Num (HaskellLiteralForQExpr b1), Num (HaskellLiteralForQExpr b2), Num (HaskellLiteralForQExpr a1), Num (HaskellLiteralForQExpr a2)
-           ) => StationStatusT f -> (t1, a1, b1) -> (t2, a2, b2) -> QGenExpr context be s Bool
-keepRow row (leadReported, leadBikes, leadDocks) (lag, lagBikes, lagDocks) =
-  keepForEmpty row (leadReported, leadBikes, leadDocks) (lag, lagBikes, lagDocks) ||.
-  keepForFull  row (leadReported, leadBikes, leadDocks) (lag, lagBikes, lagDocks)
+-- * Functions to determine if a row should be considered in occupancy calculation.
+
+keepRow :: (SqlValable (Columnar f Int32), SqlOrd (QGenExpr ctx Postgres s) (Columnar f Int32))
+        => HaskellLiteralForQExpr (Columnar f Int32)
+        -> HaskellLiteralForQExpr (Columnar f Int32)
+        -> StationStatusT f
+        -> (C f UTCTime, C f Int32, C f Int32)
+        -> (C f UTCTime, C f Int32, C f Int32)
+        -> QGenExpr ctx Postgres s Bool
+keepRow emptyThresh fullThresh row (leadReported, leadBikes, leadDocks) (lag, lagBikes, lagDocks) =
+  keepForEmpty emptyThresh fullThresh row (leadReported, leadBikes, leadDocks) (lag, lagBikes, lagDocks) ||.
+  keepForFull  emptyThresh fullThresh row (leadReported, leadBikes, leadDocks) (lag, lagBikes, lagDocks)
 
 -- Keep row if significant to bikes available (empty: == 0) query.
-keepForEmpty :: ( BeamSqlBackend be, SqlValable b1, SqlValable b2, SqlValable a1,  SqlValable a2
-                , SqlEq (QGenExpr context be s) b1,  SqlEq (QGenExpr context be s) b2, SqlEq (QGenExpr context be s) a1,  SqlEq (QGenExpr context be s) a2
-                , SqlEq (QGenExpr context be s) (Columnar f Int32)
-                , Num (Columnar f Int32), Num (HaskellLiteralForQExpr b1), Num (HaskellLiteralForQExpr b2), Num (HaskellLiteralForQExpr a1), Num (HaskellLiteralForQExpr a2)
-                ) => StationStatusT f -> (t1, a1, b1) -> (t2, a2, b2) -> QGenExpr context be s Bool
-keepForEmpty row (_leadReported, leadBikes, _leadDocks) (_lagReported, lagBikes, _lagDocks) =
+keepForEmpty :: (SqlValable (Columnar f Int32), SqlOrd (QGenExpr ctx Postgres s) (Columnar f Int32))
+             => HaskellLiteralForQExpr (Columnar f Int32)
+             -> HaskellLiteralForQExpr (Columnar f Int32)
+             -> StationStatusT f
+             -> (C f UTCTime, C f Int32, C f Int32)
+             -> (C f UTCTime, C f Int32, C f Int32)
+             -> QGenExpr ctx Postgres s Bool
+keepForEmpty emptyThresh _fullThresh row (_leadReported, leadBikes, _leadDocks) (_lagReported, lagBikes, _lagDocks) =
   isEmpty ||. (notEmpty &&. prevEmpty)
-  where isEmpty       = row ^. statusNumBikesAvailable ==. 0
+  where isEmpty       = (row ^. statusNumBikesAvailable) <=. val_ emptyThresh
         notEmpty      = not_ isEmpty
-        prevEmpty     = lagBikes  ==. val_ 0
-        _nextNotEmpty = not_ (leadBikes ==. val_ 0)
+        prevEmpty     = lagBikes  <=. val_ emptyThresh
+        _nextNotEmpty = not_ (leadBikes <=. val_ emptyThresh)
 
 -- Keep row if significant to docks available (full: == 0) query.
-keepForFull :: ( BeamSqlBackend be, SqlValable b1, SqlValable b2, SqlValable a1,  SqlValable a2
-               , SqlEq (QGenExpr context be s) b1,  SqlEq (QGenExpr context be s) b2, SqlEq (QGenExpr context be s) a1,  SqlEq (QGenExpr context be s) a2
-               , SqlEq (QGenExpr context be s) (Columnar f Int32)
-               , Num (Columnar f Int32), Num (HaskellLiteralForQExpr b1), Num (HaskellLiteralForQExpr b2), Num (HaskellLiteralForQExpr a1), Num (HaskellLiteralForQExpr a2)
-               ) => StationStatusT f -> (t1, a1, b1) -> (t2, a2, b2) -> QGenExpr context be s Bool
-keepForFull row (_leadReported, _leadBikes, leadDocks) (_lagReported, _lagBikes, lagDocks) =
+keepForFull :: (SqlValable (Columnar f Int32), SqlOrd (QGenExpr ctx Postgres s) (Columnar f Int32))
+            => HaskellLiteralForQExpr (Columnar f Int32)
+            -> HaskellLiteralForQExpr (Columnar f Int32)
+            -> StationStatusT f
+            -> (C f UTCTime, C f Int32, C f Int32)
+            -> (C f UTCTime, C f Int32, C f Int32)
+            -> QGenExpr ctx Postgres s Bool
+keepForFull _emptyThresh fullThresh row (_leadReported, _leadBikes, leadDocks) (_lagReported, _lagBikes, lagDocks) =
   isFull ||. (notFull &&. prevFull)
-  where isFull       = row ^. statusNumDocksAvailable ==. 0
+  where isFull       = row ^. statusNumDocksAvailable <=. val_ fullThresh
         notFull      = not_ isFull
-        prevFull     = lagDocks  ==. val_ 0
-        _nextNotFull = not_ (leadDocks ==. val_ 0)
+        prevFull     = lagDocks  <=. val_ fullThresh
+        _nextNotFull = not_ (leadDocks <=. val_ fullThresh )
 
 
--- insertStationOccupancy :: MonadBeam Postgres m => m [(StationInformationT Identity, (Maybe Int32, Maybe Int32))]
--- insertStationOccupancy = do
---     insert (bikeshareDb ^. bikeshareStationOccupancy)
---     (insertExpressions [])
+-- * Functions for querying station occupancy and caching results.
 
-toStationOccupancy :: StationInformationT Identity
-                   -> UTCTime -- ^ Time calculation was done
-                   -> UTCTime -- ^ Starting bound of range considered.
-                   -> UTCTime -- ^ Ending   bound of range considered.
-                   -> Int32   -- ^ Minimum number of available bikes to be considered empty.
-                   -> Int32   -- ^ Minimum number of available docks to be considered full.
-                   -> Int32   -- ^ Number of seconds the station was empty.
-                   -> Int32   -- ^ Number of seconds the station was full.
-                   -> StationOccupancy
-toStationOccupancy info calcT startT endT emptyThr fullThr secEmpty secFull =
-  StationOccupancy { _stnOccInfo        = primaryKey info
-                   , _stnOccCalculated  = calcT
-                   , _stnOccRangeStart  = startT
-                   , _stnOccRangeEnd    = endT
-                   , _stnOccEmptyThresh = emptyThr
-                   , _stnOccFullThresh  = fullThr
-                   , _stnOccEmptySec    = secEmpty
-                   , _stnOccFullSec     = secFull
-                   }
+cacheStationOccupancy :: (MonadBeamInsertReturning Postgres m)
+                      => Int32
+                      -> Int32
+                      -> Maybe Int32
+                      -> UTCTime
+                      -> UTCTime
+                      -> m [StationOccupancyT Identity]
+cacheStationOccupancy emptyThresh fullThresh stationId startT endT = do
+  runInsertReturningList $
+    insert (bikeshareDb ^. bikeshareStationOccupancy) $
+    insertFrom $ do
+      (info, (empty, full)) <- stationOccupancyThreshE 0 0 stationId startT endT
+      pure $ StationOccupancy (primaryKey info) default_ (val_ startT) (val_ endT) (val_ emptyThresh) (val_ fullThresh) (fromMaybe_ 0 empty) (fromMaybe_ 0 full)
