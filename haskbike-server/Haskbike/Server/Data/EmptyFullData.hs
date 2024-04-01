09b577c1ba3794693248214fc2fa30bfd9851eb1
Connor Feeley <git@cfeeley.org>
2024-04-01 09:00:00 -0400

    server(e/f): remove unused imports

--- haskbike-server/Haskbike/Server/Data/EmptyFullData.hs
+++ haskbike-server/Haskbike/Server/Data/EmptyFullData.hs
@@ -7,19 +7,14 @@ module Haskbike.Server.Data.EmptyFullData
 
 import           Control.Arrow                               ( first )
 
-import           Data.Aeson                                  ( ToJSON (..), object, (.=) )
 import qualified Data.Map                                    as Map
 import           Data.Maybe                                  ( mapMaybe )
 import           Data.String                                 ( IsString )
 import qualified Data.Text                                   as T
 import           Data.Time
 
-import           GHC.Generics                                ( Generic )
-
-import           Haskbike.Database.Tables.StationInformation ( fromBeamStationInformationToJSON )
 import qualified Haskbike.Database.Tables.StationInformation as DB
 import qualified Haskbike.Database.Tables.StationOccupancy   as DB
-import           Haskbike.Database.Tables.StationStatus      ( fromBeamStationStatusToJSON )
 import qualified Haskbike.Database.Tables.StationStatus      as DB
 
 
