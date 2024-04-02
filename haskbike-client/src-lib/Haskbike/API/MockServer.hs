5a97f78915afeecfd74c74e7d373a958cceb1aaf
Connor Feeley <git@cfeeley.org>
2024-04-02 09:00:00 -0400

    client(mock): add vehicle type data

--- haskbike-client/src-lib/Haskbike/API/MockServer.hs
+++ haskbike-client/src-lib/Haskbike/API/MockServer.hs
@@ -19,6 +19,7 @@ import           Haskbike.API.APIVersion
 import           Haskbike.API.BikeShareAPI
 import           Haskbike.API.ResponseWrapper
 import           Haskbike.API.SystemInformation
+import           Haskbike.API.VehicleTypeFull
 import           Haskbike.AppEnv
 
 import           Network.Wai.Handler.Warp             as Warp
@@ -33,41 +34,11 @@ import           Servant.Server.Generic
 import           UnliftIO
 
 
-mkResponseWrapper dat = do
-  ct <- liftIO getCurrentTime
-  pure $
-    ResponseWrapper { _respLastUpdated = ct
-                    , _respTtl         = 35
-                    , _respVer         = "2.3"
-                    , _respData        = dat
-                    }
-
-mockVersions = [ APIVersion { _apiVersion = 2.3, _apiUrl = "localhost" } ]
-
-mockSysInf =
-  SystemInformation
-  { _sysInfStationCount = 792
-  , _sysInfVehicleCount = SystemInformationVehicleCount
-                          { sysInfMechanicalCount = 7907
-                          , sysInfEbikeCount      =  887
-                          }
-  , _sysInfBuildHash            = "432ff52"
-  , _sysInfBuildLabel           = "2024-01-29"
-  , _sysInfBuildNumber          = "40"
-  , _sysInfBuildVersion         = "2023.2"
-  , _sysInfLanguage             = "en"
-  , _sysInfMobileHeadVersion    = 2
-  , _sysInfMobileMinSuppVersion = 1
-  , _sysInfName                 = "bike_share_toronto"
-  , _sysInfSysId                = "bike_share_toronto"
-  , _sysInfTimeZone             = "America/Toronto"
-  }
-
-
+-- | Mock server handlers.
 mockServer :: ( WithEnv (Env m) m, WithEnv (Env m) m )
            => BikeShareAPIRoutes (AsServerT m)
 mockServer = BikeShareAPIRoutes { _versions           = mkResponseWrapper mockVersions
-                                , _vehicleTypes       = mkResponseWrapper []
+                                , _vehicleTypes       = mkResponseWrapper mockVehicleTypes
                                 , _stationInformation = mkResponseWrapper []
                                 , _stationStatus      = mkResponseWrapper []
                                 , _systemRegions      = mkResponseWrapper []
@@ -75,6 +46,7 @@ mockServer = BikeShareAPIRoutes { _versions           = mkResponseWrapper mockVe
                                 , _systemPricingPlans = mkResponseWrapper []
                                 }
 
+-- | Run the mock server.
 runMockServer :: (HasEnv (Env AppM) m, MonadCatch m, MonadUnliftIO m, MonadFail m)
               => Int -> m ()
 runMockServer port = do
@@ -98,8 +70,54 @@ serverNt env action =
     exceptionHandler :: SomeException -> IO a
     exceptionHandler ex = throwM (servantErrFromEx ex)
 
+-- | Error handler.
 servantErrFromEx :: SomeException -> S.ServerError
 servantErrFromEx _ex = S.err500 { S.errBody = "Internal server error" }
 
+-- | Generic server representation.
 serverApp :: Env AppM -> S.Application
 serverApp state = S.genericServeT (serverNt state) mockServer
+
+
+-- * Mock data.
+
+-- | Create a 'ResponseWrapper' with some defaults.
+mkResponseWrapper dat = do
+  ct <- liftIO getCurrentTime
+  pure $
+    ResponseWrapper { _respLastUpdated = ct
+                    , _respTtl         = 35
+                    , _respVer         = "2.3"
+                    , _respData        = dat
+                    }
+
+-- | Mock response for 'APIVersion' endpoint.
+mockVersions = [ APIVersion { _apiVersion = 2.3, _apiUrl = "localhost" } ]
+
+mockVehicleTypes =
+  [ VehicleTypeFull { vehicleTypeFullId = Iconic, vehicleTypeFullFormFactor = "bicycle", vehicleTypeFullPropulsion = "human",           vehicleTypeFullMaxRange = 0.0,     vehicleTypeFullName = "ICONIC",  vehicleTypeFullPricingPlan = "186-1" }
+  , VehicleTypeFull { vehicleTypeFullId = Fit   , vehicleTypeFullFormFactor = "bicycle", vehicleTypeFullPropulsion = "human",           vehicleTypeFullMaxRange = 0.0,     vehicleTypeFullName = "FIT",     vehicleTypeFullPricingPlan = "186-1" }
+  , VehicleTypeFull { vehicleTypeFullId = Boost , vehicleTypeFullFormFactor = "bicycle", vehicleTypeFullPropulsion = "electric_assist", vehicleTypeFullMaxRange = 0.0,     vehicleTypeFullName = "BOOST",   vehicleTypeFullPricingPlan = "186-2" }
+  , VehicleTypeFull { vehicleTypeFullId = EFit  , vehicleTypeFullFormFactor = "bicycle", vehicleTypeFullPropulsion = "electric_assist", vehicleTypeFullMaxRange = 0.0,     vehicleTypeFullName = "EFIT",    vehicleTypeFullPricingPlan = "186-2" }
+  , VehicleTypeFull { vehicleTypeFullId = EFitG5, vehicleTypeFullFormFactor = "bicycle", vehicleTypeFullPropulsion = "electric_assist", vehicleTypeFullMaxRange = 60000.0, vehicleTypeFullName = "EFIT G5", vehicleTypeFullPricingPlan = "186-2" }
+  ]
+
+-- | Mock response for 'SystemInformation' endpoint.
+mockSysInf =
+  SystemInformation
+  { _sysInfStationCount = 792
+  , _sysInfVehicleCount = SystemInformationVehicleCount
+                          { sysInfMechanicalCount = 7907
+                          , sysInfEbikeCount      =  887
+                          }
+  , _sysInfBuildHash            = "432ff52"
+  , _sysInfBuildLabel           = "2024-01-29"
+  , _sysInfBuildNumber          = "40"
+  , _sysInfBuildVersion         = "2023.2"
+  , _sysInfLanguage             = "en"
+  , _sysInfMobileHeadVersion    = 2
+  , _sysInfMobileMinSuppVersion = 1
+  , _sysInfName                 = "bike_share_toronto"
+  , _sysInfSysId                = "bike_share_toronto"
+  , _sysInfTimeZone             = "America/Toronto"
+  }
