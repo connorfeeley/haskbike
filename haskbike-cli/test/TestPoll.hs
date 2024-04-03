41007cf370fd22856925afd344e59487f64fd255
Connor Feeley <git@cfeeley.org>
2024-04-03 09:00:00 -0400

    cli(test): add test for poll queue insertion

--- haskbike-cli/test/TestPoll.hs
+++ haskbike-cli/test/TestPoll.hs
@@ -4,50 +4,97 @@ module TestPoll
      ( tests
      ) where
 
-import           Control.Monad                ( void )
-import           Control.Monad.Reader         ( runReaderT )
+import           Colog
 
-import           Haskbike.API.MockServer      ( mockServerBaseUrl, runMockServer )
+import           Control.Monad
+import           Control.Monad.Catch               ( MonadCatch, MonadThrow )
+import           Control.Monad.Reader              ( runReaderT )
+
+import qualified Data.Text                         as T
+
+import           Database.Beam
+
+import           Haskbike.API.APIEntity
+import qualified Haskbike.API.Client               as C
+import           Haskbike.API.MockServer           ( MockServerLog (MockServerLogNever), mockServerBaseUrl,
+                                                     runMockServer )
 import           Haskbike.AppEnv
-import           Haskbike.CLI.Options         ( PollOptions (..), PopulateStatusChangesOpt (..) )
-import qualified Haskbike.CLI.Poll            as Poll
-import           Haskbike.Database.BikeShare  ( migrateDB )
+import           Haskbike.CLI.Options              ( PollOptions (..), PopulateStatusChangesOpt (..) )
+import           Haskbike.CLI.Poll
+import           Haskbike.Database.BikeShare
+import           Haskbike.Database.EndpointQueried
+import           Haskbike.Database.Operations      ( lockTableAndSleep, queryRowCount )
 import           Haskbike.Database.Test.Utils
 
-import           Prelude                      hiding ( log, unwords )
+import           Prelude                           hiding ( log, unwords )
 
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
-import           UnliftIO                     ( concurrently, liftIO, timeout )
+import           UnliftIO
 
 
 tests :: TestTree
 tests = testGroup "Polling tests"
-  [ testCase "Poll API"      poll
+  [ testCase "Poll API"      pollApi
   , testCase "Poll mock API" pollMock
+  , testCase "Queued records are inserted after table is unlocked" pollMockAndTestQueueing
   ]
 
 
-poll :: IO ()
-poll = do
-  withTempDbM Silent migrateDB doPoll
-  where
-    pollOpts = PollOptions NeverPopulate
+-- | Test polling the PBSC API.
+pollApi :: IO ()
+pollApi = withTempDbM Silent migrateDB $
+  void $ timeout (5 * 1000000) (pollClient (PollOptions NeverPopulate))
 
-    doPoll :: AppM ()
-    doPoll = void $ timeout (5 * 1000000) $ -- Terminate after 5 seconds
-             Poll.pollClient pollOpts
-
+-- | Test polling the mock API.
 pollMock :: IO ()
-pollMock = do
-  withTempDbM Silent migrateDB $ do
-    env <- ask
-    liftIO $ runReaderT (unAppM doPoll) (env { envBaseUrl = mockServerBaseUrl })
+pollMock = withTempDbM Silent migrateDB $ do
+  -- Update default environment to poll local mock server.
+  env <- ask
+  let envMock = env { envBaseUrl = mockServerBaseUrl }
+  liftIO $ runReaderT (unAppM doPoll) envMock
   where
-    pollOpts = PollOptions NeverPopulate
+    doPoll = void $ timeout (5 * 1000000) (pollClient (PollOptions NeverPopulate))
 
+{- | Poll mock info API and queue records for insertion, but lock info table.
+Checks that queued records are all inserted after table is unlocked.
+-}
+pollMockAndTestQueueing :: IO ()
+pollMockAndTestQueueing = withTempDbM Silent migrateDB $ do
+  -- Update default environment to poll local mock server.
+  env <- ask
+  let envMock = env { envBaseUrl = mockServerBaseUrl, envMinSeverity = Info, envLogDatabase = False }
+  liftIO $ runReaderT (unAppM doPoll) envMock
+  where
     doPoll :: AppM ()
-    doPoll = void $ timeout (5 * 1000000) $ -- Terminate after 5 seconds
-             concurrently (runMockServer 8082)
-                          (Poll.pollClient pollOpts)
+    doPoll = void (concurrently (timeout (10 * 1000000) (runMockServer MockServerLogNever 8082))
+                                (concurrently (lockInfoTable 5)
+                                              pollInfoOnly
+                                )
+                  )
+
+lockInfoTable :: (HasEnv env m, MonadUnliftIO m, MonadCatch m) => Int -> m ()
+lockInfoTable duration = do
+  logWarning . T.pack $ "Locking " <> tableName <> " for " <> show duration <> " seconds."
+  lockTableAndSleep tableName duration
+  where tableName = "station_information"
+
+pollInfoOnly :: (HasEnv env m, MonadIO m, MonadThrow m, MonadCatch m, MonadUnliftIO m) => m ()
+pollInfoOnly = do
+  -- Create insertion and polling threads.
+  logInfo "Initializing insertion and polling threads."
+  lastUpdated  <- liftIO $ newTVarIO 0
+  queue        <- liftIO newTQueueIO
+  -- Run each action 5 times.
+  insertThreadInfo <- (async . replicateM_ iterations) (insertThread StationInformationEP queue)
+  pollThreadInfo   <- (async . replicateM_ iterations) (pollThread StationInformationEP C.stationInformation lastUpdated queue)
+
+  -- Start both threads and wait for both to finish.
+  _ <- waitBoth insertThreadInfo pollThreadInfo
+  logInfo "Polling done"
+
+  infoRowCount <- queryRowCount bikeshareStationInformation
+  liftIO $ assertEqual "Inserted all blocked station information." (iterations * 1000) infoRowCount
+  where
+    iterations = 5
